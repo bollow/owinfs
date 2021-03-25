20c1dd1b716ecb6470620898a2c4f9e3ef948f68
Norbert Bollow <nb@bollow.ch>
2021-03-25 12:40:51 +0100

    Fisheries page update

--- fish.php
+++ fish.php
@@ -14,12 +14,12 @@ include './navbar.php';
 
         <div id="content-header">
           <h1 class="title">Fisheries negotiations at WTO</h1>
-          <h2>Statements and papers from civil society</h2>
         </div> <!-- /#content-header -->
         
         <div id="content-area">
-
-<h3>2020</h3>
+<h2>Current statements and analysis from civil society</h2>
+<a href="2021-03-17_TWN_Fisheries">Protecting the Problem – Fisheries subsidies negotiations and the WTO</a> (17 March 2021)
+<p>
 <i>Call to Halt Fisheries Subsidies Negotiations in the Middle of the COVID 19 Crisis</i>:
       <a href="/2020/Fisheries_subsidies.pdf">English</a>,
       <a href="/2020/Fisheries_subsidies_ES.pdf">Spanish</a>,
@@ -27,18 +27,18 @@ include './navbar.php';
       <a href="/2020/Fisheries_subsidies_REPLY.pdf">Response from WTO DG Azevêdo</a>.
 <p>
 
-<h3>Analysis papers from 2019</h3>
-<a href="/2019/PANG_fisheries.pdf"><i>Fisheries subsidies negotiations and development in the World Trade Organization</i></a>
-
-<h3>Analysis papers from 2018</h3>
+<h2>Older but still relevant</h2>
+<a href="/2019/PANG_fisheries.pdf"><i>Fisheries subsidies negotiations and development in the World Trade Organization</i></a> (2019)
+<p>
 <i>Market access the true aim of WTO fisheries subsidies negotiations</i>:
 <a href="https://ourworldisnotforsale.net/2018/ALAI_fisheries_subsidies.pdf">English</a>, 
 <a href="https://ourworldisnotforsale.net/2018/ALAI_subvenciones_pesca.pdf">Spanish</a>
-
-<h3>Statements and analysis from 2017</h3>
+(2018)
+<p>
 Quote sheet and video: <i>Civil Society Experts Outline What Is Needed to Protect the Development Mandate in Agriculture and Fisheries-Subsidies Negotiations in MC11:</i>
 <a href="https://ourworldisnotforsale.net/2017/R_agriculture_fisheries.pdf">English</a>,
 <a href="https://ourworldisnotforsale.net/2017/R_agriculture_fisheries_ES.pdf">Spanish</a>.
+(2017)
 <p>
 <i>Analysis regarding fisheries subsidies negotiations:</i>
 English:
@@ -50,6 +50,7 @@ French:
 Spanish:
 <a href="https://ourworldisnotforsale.net/2017/PANG_Fisheries_ES.pdf">pdf</a>,
 <a href="https://ourworldisnotforsale.net/2017/PANG_Fisheries_ES.doc">doc</a>.
+(2017)
 <p>
         </div> <!-- /#content-area -->
       </div> <!-- /#content -->
