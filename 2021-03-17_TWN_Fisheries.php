4d52249d167134dddb9bded681e969d1ed00c3a2
Norbert Bollow <nb@bollow.ch>
2021-03-25 12:41:18 +0100

    Fisheries page update

---
+++ 2021-03-17_TWN_Fisheries.php
@@ -0,0 +1,60 @@
+#!/usr/bin/php
+<?php
+$html_title="TWN Info: Protecting the Problem – Fisheries subsidies negotiations and the WTO";
+include './head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$sidebar_title="About us";
+include './sidebar_about.php';
+include './navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+        TWN Info Service on WTO and Trade Issues<br>
+        17 March 2021<br>
+        <a href="https://www.twn.my/">Third World Network</a>
+        <div id="content-header">
+          <h1 class="title">Protecting the Problem</h1>
+	  <h2>Fisheries subsidies negotiations and the WTO</h2>
+	  <h4>by Adam Wolfenden and Ranja Sengupta</h4>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+This year the World Trade Organization (WTO) is aiming to reach an agreement on banning certain types of subsidies for fisheries. This comes at a time when global fish stocks are in crisis with a third of global stocks already being fished beyond their sustainable levels. The Sustainable Development Goal 14.6 has the target to “prohibit certain forms of fisheries subsidies which contribute to overcapacity and overfishing and eliminate subsidies that contribute to Illegal, Unreported and Unregulated fishing” whilst ensuring “appropriate and effective special and differential treatment (SDT) for developing and least-developed countries should be an integral part” of negotiations. This mandate however is being re-interpreted within the WTO to protect the major subsidisers whilst targeting any subsidies by developing or least-developed countries.
+<p>
+The SDG mandate implicitly calls for developed countries to take responsibility for past extraction of the marine resources and take on strong commitments to ensure that any disciplines on fisheries subsidies actually meets the objective it was set out for. The principle of Common but Differentiated Responsibility (CBDR), that includes SDT but goes beyond, underpins this idea and is now the current ask from most developing countries. It is only fair that richer countries with larger-scale and industrial fishing take on higher commitments. Yet despite this situation the biggest subsidisers are being gifted a permanent carve-out under the current proposals meaning those most responsible are shouldering the least burden.
+<p>
+In the latest revision of the Chairs text (18th December 2020) a list of prohibited subsidies are listed regarding Overcapacity and Overfishing (Article 5.1). These include subsidies for vessels (building, buying upgrading etc), buying of machines or equipment (fishing gear, engines, fish-finding technology and others), buying fuel, ice or bait, plus a number of other types of subsidies.
+<p>
+However this is followed by Article 5.2, which states that a Member may grant or maintain subsidies that are prohibited “if it demonstrates that measures are implemented to maintain the stock or stocks in the relevant fishery or fisheries at a biologically sustainable level.” It also contains a footnote detailing how the biologically sustainable level can be determined.
+<p>
+Allowing the continuation of subsidies provided that there are management measures (necessary for complying with Art. 5.2) creates a number of problems both for the conservation of fish resources and the ability of developing countries to be able to develop industries to fish their own waters.
+<p>
+While the big subsidisers like the EU, USA and Japan have already built their fleet capacity and are responsible for the industrial overfishing of fish stocks in some cases to a point of collapse, the proposed text leaves it open for them to continue to subsidise as they please. Those countries with the financial resourcing to subsidise as well as the technical capacity to monitor, collate data and manage their fisheries will not be bound by these prohibitions. This means that one of the major components of the SDG mandate will not be imposed on those most responsible for the state of global fish stocks. Interestingly, Article 5.2 does not even contain any conditionality or qualification that pins down larger countries with very large-scale fishing operations, and which enjoy massive over-capacity.
+<p>
+Therefore, Article 5.2 will further exacerbate the inequity among developed and developing countries, and lead to a reverse SDT for developed countries allowing them to use their technical capacity to monitor, measure and prove they comply with 5.2, while developing countries, who actually meet it more easily, will find it difficult to prove that they do so.
+<p>
+The basis for such text has unsurprisingly come from, and is supported by, some of the biggest subsidisers who desperately want to maintain the ability to subsidise their fleets. Not only that it also opens the door for them to challenge and undermine the ability of other WTO Members to manage their fish stocks.
+<p>
+The article as currently proposed asks the WTO to act as an arbiter of a Member’s fisheries conservation and management measures, a responsibility it is neither mandated nor expert enough to take on. The requirement that Members must ‘demonstrate’ that the measures are implemented opens up the space for those measures to be challenged and sets a dangerous precedent to make this binding in a multilateral forum. There are already examples of developed nations challenging developing country management measures like the EU’s uniliateral red flag warnings and bans on imports from those countries as well as New Zealand challenging the highly successful management and revenue generating measures of some Pacific Island Countries.
+<p>
+Fisheries management measures are complex and contested but ultimately the realm of the coastal states who have sovereignty and obligation to conserve and manage their natural resources under the United Nations Convention on the Law of the Sea.
+<p>
+The current text also fails the ‘special and differential treatment’ (SDT) mandate of the SDG, a principle that ensures that developing and least-developed countries take on lower commitments due to their level of development and capacity. Current proposals on special and differential treatment on overcapacity and overfishing have some carve-outs for Least-Developed Countries and developing countries but these are transitional and too limited in geographic scope. These proposals are too weak to be of practical use for developing countries and instead need to be made permanent, expanding to the full exclusive economic zone.
+<p>
+Finally, there are capping proposals in the offing that benefit the large subsidisers much more as they can “cap” their current levels of subsidies which are much higher than what most developing countries are giving, but may not have to reduce or eliminate subsidies below these caps. While these proposals have not been prominently visible yet, they are on a placeholder in the text and may be sprung at the last moment in order to protect developed country subsidies. This would be equal to repeating the monumental blunder of the Agreement on Agriculture (AoA), which had allowed rich country subsidies to continue under different garbs, while banning developing countries from subsidising their farm sector. This will again amount to a reverse SDT for richer countries.
+<p>
+The long-term sustainability of fisheries is a complex and multidimensional challenge that is being faced throughout the world. Addressing the issue of subsidies that contribute to overcapacity and overfishing are important steps but they must be done properly which means ensuring that those most responsible for the problem shoulder the most responsibility while allowing those countries who hold the resources, often the same countries who don’t have the money to support their own domestic industries, to develop. Allowing a reverse-SDT through multiple mechanisms where the big fishing nations will effectively be able to avoid having to make any prohibitions on their subsidies that enhance rather than limit their fishing capacity, will be a monumental failure of the WTO to actually deliver on its mandate. It is also time for developing countries to come together in solidarity and articulate a strong demand from rich countries to meet their responsibilities; the latter must not commit to anything less.
+<p>
+&nbsp;
+<p>
+<b>About the authors:</b><br>
+Adam Wolfenden is the Trade Justice Campaigner with Pacific Network on Globalisation (PANG)<br>
+Ranja Sengupta is Senior Researcher with Third World Network (TWN).
+</div> <!-- /#content-area -->
+</div> <!-- /#content -->
+</div> <!-- /#main -->
+<?php
+include './foot_etc.php'
+?>
