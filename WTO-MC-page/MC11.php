cde50988048d41cb994b703baec116d04389dcfe
Norbert Bollow <nb@bollow.ch>
2021-03-29 15:31:18 +0200

    Fixed links

--- WTO-MC-page/MC11.php
+++ WTO-MC-page/MC11.php
@@ -25,39 +25,39 @@ Post-Ministerial Analysis:
 <a href="https://ourworldisnotforsale.net/2018/ALAI_cumbre_pueblos.pdf">Spanish</a>
     <p>
     <p>
-<b><a href="http://ourworldisnotforsale.net/2017/OWINFS_Flyer.pdf">Our World is Not for Sale (OWINFS) one-page flyer</a></b>
+<b><a href="https://ourworldisnotforsale.net/2017/OWINFS_Flyer.pdf">Our World is Not for Sale (OWINFS) one-page flyer</a></b>
 
 <h2>Media information</h2>
 
 December 13, 2017<br />
 <b><i>Our World is Not for Sale (OWINFS) Final Statement on MC11:</i></b>
-<a href="http://ourworldisnotforsale.net/2017/OWINFS_final_stmt.pdf">English</a>,
-<a href="http://ourworldisnotforsale.net/2017/OWINFS_final_stmt_ES.pdf">Spanish</a>.
+<a href="https://ourworldisnotforsale.net/2017/OWINFS_final_stmt.pdf">English</a>,
+<a href="https://ourworldisnotforsale.net/2017/OWINFS_final_stmt_ES.pdf">Spanish</a>.
 </p><p>
 Media release (Dec 13, 2017):<br /><b><i>Global civil society reactions to end of WTO ministerial conference:</i></b>
-<a href="http://ourworldisnotforsale.net/2017/R_reactions.pdf">English</a>,
-<a href="http://ourworldisnotforsale.net/2017/R_reactions_ES.pdf">Spanish</a>.
+<a href="https://ourworldisnotforsale.net/2017/R_reactions.pdf">English</a>,
+<a href="https://ourworldisnotforsale.net/2017/R_reactions_ES.pdf">Spanish</a>.
 </p><p>
 Media release (Dec 13, 2017):<br /><b><i>On last day of WTO Ministerial, civil society distributes urgent recommendations to delegates attending heads of delegation meetings:</i></b>
-<a href="http://ourworldisnotforsale.net/2017/R_recommend.pdf">English</a>,
-<a href="http://ourworldisnotforsale.net/2017/R_recommend_ES.pdf">Spanish</a>.
+<a href="https://ourworldisnotforsale.net/2017/R_recommend.pdf">English</a>,
+<a href="https://ourworldisnotforsale.net/2017/R_recommend_ES.pdf">Spanish</a>.
 </p><p>
 
 Media release (Dec 11, 2017):<br />
-<b><i><a href="http://ourworldisnotforsale.net/2017/R_protest.pdf">Civil Society Protest at the Entrance of the WTO 11<sup>th</sup> Ministerial in Buenos Aires</a></i></b>
+<b><i><a href="https://ourworldisnotforsale.net/2017/R_protest.pdf">Civil Society Protest at the Entrance of the WTO 11<sup>th</sup> Ministerial in Buenos Aires</a></i></b>
 </p><p>
-<b>MEDIA AVAILABILITY: <a href="http://ourworldisnotforsale.net/2017/R_CSO_Press_contacts.pdf">Civil Society / NGO Contacts for MC11 WTO Ministerial Dec 10-13, 2017</a></b>
+<b>MEDIA AVAILABILITY: <a href="https://ourworldisnotforsale.net/2017/R_CSO_Press_contacts.pdf">Civil Society / NGO Contacts for MC11 WTO Ministerial Dec 10-13, 2017</a></b>
 </p><p>
 <b>Global Civil Society Shares What’s at Stake at MC 11: </b>
 <ul>
-  <li><b><a href="http://ourworldisnotforsale.net/2017/R_OWINFS_gender.pdf">Quote sheet on the <i>rejection of the WTO Declaration on “Women’s Economic Empowerment”</i></a></b>
- </li><li><b><a href="http://ourworldisnotforsale.net/2017/R_agriculture_fisheries.pdf">Quote sheet and video: <i>Civil Society Experts Outline What Is Needed to Protect the Development Mandate in Agriculture and Fisheries-Subsidies Negotiations in MC11</i></a></b>
-  </li><li><b><a href="http://ourworldisnotforsale.net/2017/R_OWINFS_investment.pdf">Quote sheet: <i>‘Investment facilitation for development’ is a Trojan horse for
+  <li><b><a href="https://ourworldisnotforsale.net/2017/R_OWINFS_gender.pdf">Quote sheet on the <i>rejection of the WTO Declaration on “Women’s Economic Empowerment”</i></a></b>
+ </li><li><b><a href="https://ourworldisnotforsale.net/2017/R_agriculture_fisheries.pdf">Quote sheet and video: <i>Civil Society Experts Outline What Is Needed to Protect the Development Mandate in Agriculture and Fisheries-Subsidies Negotiations in MC11</i></a></b>
+  </li><li><b><a href="https://ourworldisnotforsale.net/2017/R_OWINFS_investment.pdf">Quote sheet: <i>‘Investment facilitation for development’ is a Trojan horse for
 investment rules in WTO, say civil society advocates</i></a></b>
-  </li><li><b><a href="http://ourworldisnotforsale.net/2017/R_OWINFS_new_issues.pdf">Quote sheet: <i>Public interest legal experts warn of dangers of
+  </li><li><b><a href="https://ourworldisnotforsale.net/2017/R_OWINFS_new_issues.pdf">Quote sheet: <i>Public interest legal experts warn of dangers of
 WTO proposals on e-commerce, domestic regulation disciplines, investment
 facilitation and micro, small and medium enterprises (MSMEs)</i></a></b>
-  </li><li><a href="http://ourworldisnotforsale.net/2017/R_OWINFS_quote_sheet.pdf"><b><i>Quotes from Civil Society Experts</i></b></a>
+  </li><li><a href="https://ourworldisnotforsale.net/2017/R_OWINFS_quote_sheet.pdf"><b><i>Quotes from Civil Society Experts</i></b></a>
  </li><li><b><a href="https://www.facebook.com/owinfs/videos/1866417760338208/">Video from the press conference</a></b>
 </li></ul>
 </p><p>
@@ -67,96 +67,96 @@ facilitation and micro, small and medium enterprises (MSMEs)</i></a></b>
 </p><p>
 <b>OWINFS side event:</b>
 <ul>
-  <li>Tuesday, December 12, 10:15 – 11.45am: <a href="http://ourworldisnotforsale.net/2017/E_Agriculture.pdf">Protecting the Development Mandate in Agriculture and Fisheries-Subsidies negotiations in MC11</a>
+  <li>Tuesday, December 12, 10:15 – 11.45am: <a href="https://ourworldisnotforsale.net/2017/E_Agriculture.pdf">Protecting the Development Mandate in Agriculture and Fisheries-Subsidies negotiations in MC11</a>
 </li></ul>
 
 <hr />
 <h2>Argentine Government Revokes World Trade Organization’s Accreditation of Key Civil Society Organizations, Just Days before Ministerial Conference in Buenos Aires</h2>
 November 30, 2017: In an unprecedented action, the Argentine government has revoked the accreditation of 63 civil society experts -- trade unionists, development advocates, digital rights activists, environmentalists, and others -- just days before the 11th Ministerial meeting of the WTO (MC11) in Buenos Aires, advising the WTO that the experts will not be allowed in the country to participate in the meeting. The majority of the rejected organizations work together through the global Our World Is Not for Sale (OWINFS) network.
 </p><p>
-Full media advisory: <a href="http://notforsale.activism.info/2017/R_Disaccreditation.docx">English</a> <a href="http://notforsale.activism.info/2017/R_Disaccreditation_ES.docx">Spanish</a> <a href="http://notforsale.activism.info/2017/R_Disaccreditation_FR.doc">French</a>
+Full media advisory: <a href="/2017/R_Disaccreditation.docx">English</a> <a href="/2017/R_Disaccreditation_ES.docx">Spanish</a> <a href="/2017/R_Disaccreditation_FR.doc">French</a>
 </p><p>
 Letter to the WTO:
-<a href="http://notforsale.activism.info/2017/L_Disaccreditation.pdf">English</a>,
-<a href="http://notforsale.activism.info/2017/L_Disaccreditation_ES.pdf">Spanish</a>.
-<a href="http://notforsale.activism.info/2017/L_Disaccreditation_FR.pdf">French</a>
+<a href="/2017/L_Disaccreditation.pdf">English</a>,
+<a href="/2017/L_Disaccreditation_ES.pdf">Spanish</a>.
+<a href="/2017/L_Disaccreditation_FR.pdf">French</a>
 </p>
 <p>
 December 2, 2017: For information on the public outcry about the above, and on how the Argentine government is justifying their actions, see e.g. here: <a href="https://www.alainet.org/en/articulo/189592">English</a>, <a href="https://www.alainet.org/es/articulo/189591">Spanish</a>.
 <p>
-<a href=".include/../owinfs-disaccreditation-followup.html">Follow-up letter</a> to Mr. Roberto Azevêdo, Director-General of the WTO (December 3, 2017)
+<a href="o/owinfs-disaccreditation-followup.html">Follow-up letter</a> to Mr. Roberto Azevêdo, Director-General of the WTO (December 3, 2017)
 </p><p>
 December 7, 2017:
-<a href="http://ourworldisnotforsale.net/2017/L_European_Commission.pdf">Letter from the European Commission</a>
+<a href="/2017/L_European_Commission.pdf">Letter from the European Commission</a>
 expressing concern and asking for the decision to be reversed.
 </p><p>
 December 8, 2017: <a href="http://www.eldisenso.com/politica/gobierno-macri-deporta-periodista-inglesa-critica-gestion-haber-entregado-informacion-argentinos-big-data">El gobierno de Macri deporta a periodista inglesa crítica de su gestión por haber entregado información de argentinos para Big Data</a> (Sally Burch deported)
 </p><p>
 December 10, 2017: Statement by Sally Burch:
-<a href="../sally-burch.html">English</a>,
-<a href="../../es/sally-burch.html">Spanish</a>.
+<a href="o/en/sally-burch.html">English</a>,
+<a href="o/es/sally-burch.html">Spanish</a>.
 </p><p>
-December 13, 2017: <a href="https://justnetcoalition.org/2017/PR_cs-rights.pdf ">Open letter on civil society rights</a>
+December 13, 2017: <a href="/2017/PR_cs-rights.pdf ">Open letter on civil society rights</a>
 
 <h2>Letter from Global Civil Society about the Agenda of the WTO</h2>
 Delivery date: 9 October, 2017
 </p><p>
 <a href="../signon/11th-wto-ministerial-letter-global-civil-society-about-agenda-wto.html">English</a>:
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda.docx">docx</a>.
+<a href="/2017/Global_MC11_agenda.pdf">pdf</a>,
+<a href="/2017/Global_MC11_agenda.docx">docx</a>.
 Spanish:
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_ES.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_ES.docx">docx</a>.
+<a href="/2017/Global_MC11_agenda_ES.pdf">pdf</a>,
+<a href="/2017/Global_MC11_agenda_ES.docx">docx</a>.
 French:
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_FR.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_FR.docx">docx</a>.
+<a href="/2017/Global_MC11_agenda_FR.pdf">pdf</a>,
+<a href="/2017/Global_MC11_agenda_FR.docx">docx</a>.
 Greek:
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_GR.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Global_MC11_agenda_GR.docx">docx</a>.
+<a href="/2017/Global_MC11_agenda_GR.pdf">pdf</a>,
+<a href="/2017/Global_MC11_agenda_GR.docx">docx</a>.
 
 
 <h2>Analysis of key issues for the 11<sup>th</sup> WTO Ministerial</h2>
 
 </p><p><!--Dec 13-->
 <b><i>MSME associations reject MSMEs work program and ecommerce rules in WTO:</i></b>
-<a href="http://ourworldisnotforsale.net/2017/R_MSMEs_reject.pdf">English</a>,
-<a href="http://ourworldisnotforsale.net/2017/R_MSMEs_reject_ES.pdf">Spanish</a>.
+<a href="/2017/R_MSMEs_reject.pdf">English</a>,
+<a href="/2017/R_MSMEs_reject_ES.pdf">Spanish</a>.
 </p><p><!--Dec 12-->
-<b><i><a href="http://ourworldisnotforsale.net/2017/Feminist_stmt.pdf">Statement by women’s rights organisations rejecting the WTO Declaration on Women’s Economic Empowerment</a></i></b>
+<b><i><a href="/2017/Feminist_stmt.pdf">Statement by women’s rights organisations rejecting the WTO Declaration on Women’s Economic Empowerment</a></i></b>
 </p><p><!--Dec 12-->
-<b>Rebuttal to “The WTO Investment Facilitation Initiative: Why It Matters” – background note: <i><a href="http://ourworldisnotforsale.net/2017/Investment_rebuttal.pdf">Investment Facilitation
+<b>Rebuttal to “The WTO Investment Facilitation Initiative: Why It Matters” – background note: <i><a href="/2017/Investment_rebuttal.pdf">Investment Facilitation
 for Development: Opening the doors of the WTO for hard rules on investment</a></i></b>
 </p><p><!--Dec 10-->
-<a href="../../gender-Trojan-horse.html"><b><i>Is “gender” a Trojan horse to introduce new issues at WTO?</i></b></a>
+<a href="o/gender-Trojan-horse.html"><b><i>Is “gender” a Trojan horse to introduce new issues at WTO?</i></b></a>
 </p><p><!--Dec 10-->
-<a href="http://ourworldisnotforsale.net/2017/IBON_economic_rights.pdf"><b><i>A deadly irony for people’s economic rights as WTO opens talks on International Rights Day</i></b></a>
+<a href="/2017/IBON_economic_rights.pdf"><b><i>A deadly irony for people’s economic rights as WTO opens talks on International Rights Day</i></b></a>
 </p><p><!--Dec 7-->
-<a href="http://ourworldisnotforsale.net/2017/TWN_DRD_analysis.pdf"><i><b>Analyses of Domestic Regulation Disciplines</b></i></a>
+<a href="/2017/TWN_DRD_analysis.pdf"><i><b>Analyses of Domestic Regulation Disciplines</b></i></a>
 </p><p><!--Dec 7-->
-<a href="http://ourworldisnotforsale.net/2017/TWN_Africa.pdf"><i><b>What Africa wants at MC11</b></i></a>
+<a href="/2017/TWN_Africa.pdf"><i><b>What Africa wants at MC11</b></i></a>
 </p><p><!--Dec 2-->
 <i><b>Fisheries subsidies negotiations</b>:</i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries.doc">doc</a>.
+<a href="/2017/PANG_Fisheries.pdf">pdf</a>,
+<a href="/2017/PANG_Fisheries.doc">doc</a>.
 French:
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries_FR.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries_FR.doc">doc</a>.
+<a href="/2017/PANG_Fisheries_FR.pdf">pdf</a>,
+<a href="/2017/PANG_Fisheries_FR.doc">doc</a>.
 Spanish:
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries_ES.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/PANG_Fisheries_ES.doc">doc</a>.
+<a href="/2017/PANG_Fisheries_ES.pdf">pdf</a>,
+<a href="/2017/PANG_Fisheries_ES.doc">doc</a>.
 </p><p><!--Dec 1-->
 <i><b>“Domestic regulation” rules in the WTO</b>:</i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/Domestic_Regulation.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Domestic_Regulation.docx">docx</a>.
+<a href="/2017/Domestic_Regulation.pdf">pdf</a>,
+<a href="/2017/Domestic_Regulation.docx">docx</a>.
 Spanish:
-<a href="http://ourworldisnotforsale.net/2017/Domestic_Regulation_ES.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Domestic_Regulation_ES.doc">doc</a>.
+<a href="/2017/Domestic_Regulation_ES.pdf">pdf</a>,
+<a href="/2017/Domestic_Regulation_ES.doc">doc</a>.
 </p><p><!--Dec 1-->
 <i><b>The fourth industrial revolution will deepen the digital divide.</b> Alliance Sud Position on the Eleventh WTO Ministerial Conference:</i>
-<a href="http://ourworldisnotforsale.net/2017/AS_Digital_divide.pdf">English</a>,
-<a href="http://ourworldisnotforsale.net/2017/AS_Digital_divide_FR.pdf">French</a>.
+<a href="/2017/AS_Digital_divide.pdf">English</a>,
+<a href="/2017/AS_Digital_divide_FR.pdf">French</a>.
 
 
 
@@ -164,57 +164,57 @@ Spanish:
 </p><p>
 <i><b>Twelve reasons to oppose rules on digital commerce in the WTO<b>:</b></b></i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons.docx">docx</a>.
+<a href="/2017/WTO_12_reasons.pdf">pdf</a>,
+<a href="/2017/WTO_12_reasons.docx">docx</a>.
 French:
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons_FR.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons_FR.docx">docx</a>.
+<a href="/2017/WTO_12_reasons_FR.pdf">pdf</a>,
+<a href="/2017/WTO_12_reasons_FR.docx">docx</a>.
 Spanish:
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons_ES.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/WTO_12_reasons_ES.doc">doc</a>.
+<a href="/2017/WTO_12_reasons_ES.pdf">pdf</a>,
+<a href="/2017/WTO_12_reasons_ES.doc">doc</a>.
 </p><p>
 <i><b>E-commerce and the World Trade Organization<b>:</b></b></i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce.docx">docx</a>.
+<a href="/2017/ALAI_E-commerce.pdf">pdf</a>,
+<a href="/2017/ALAI_E-commerce.docx">docx</a>.
 Spanish:
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce_ES.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce_FR.docx">docx</a>.
+<a href="/2017/ALAI_E-commerce_ES.pdf">pdf</a>,
+<a href="/2017/ALAI_E-commerce_FR.docx">docx</a>.
 French:
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce_FR.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/ALAI_E-commerce_FR.docx">docx</a>.
+<a href="/2017/ALAI_E-commerce_FR.pdf">pdf</a>,
+<a href="/2017/ALAI_E-commerce_FR.docx">docx</a>.
 </p><p>
-<a href="http://ourworldisnotforsale.net/2017/UNI_Digital_divide.pdf"><i><b>Risk of a deepening of the digital divide</b> – worrisome WTO discussions</i></a>
+<a href="/2017/UNI_Digital_divide.pdf"><i><b>Risk of a deepening of the digital divide</b> – worrisome WTO discussions</i></a>
 </p><p>
-<b><i><a href="http://ourworldisnotforsale.net/2017/TWN_E-commerce.pdf">Preliminary analysis of aspects of some WTO ecommerce proposals</a></i></b>
+<b><i><a href="/2017/TWN_E-commerce.pdf">Preliminary analysis of aspects of some WTO ecommerce proposals</a></i></b>
 </p><p>
-<b><i><a href="http://ourworldisnotforsale.net/2017/TWN_Source_code.pdf">Some preliminary implications of WTO source code proposal</a></i></b>
+<b><i><a href="/2017/TWN_Source_code.pdf">Some preliminary implications of WTO source code proposal</a></i></b>
 </p><p>
-<b><i><a href="http://ourworldisnotforsale.net/2017/TWN_Procurement.pdf">Some of the implications of ecommerce proposals for government procurement</a></i></b>
+<b><i><a href="/2017/TWN_Procurement.pdf">Some of the implications of ecommerce proposals for government procurement</a></i></b>
 
 </p><p>
 <i><b>Micro, small, and medium-sized enterprises (MSMEs) and e-commerce:</b></i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/Hill_MSMEs_E-commerce.pdf">pdf</a>,
-<a href="http://ourworldisnotforsale.net/2017/Hill_MSMEs_E-commerce.docx">docx</a>.
+<a href="/2017/Hill_MSMEs_E-commerce.pdf">pdf</a>,
+<a href="/2017/Hill_MSMEs_E-commerce.docx">docx</a>.
 </p><p>
-UNCTAD report: <b><i><a href="http://ourworldisnotforsale.net/2017/UNCTAD_competitiveness.pdf">Rising product digitalisation and losing trade competitiveness</a>.</i></b>
+UNCTAD report: <b><i><a href="/2017/UNCTAD_competitiveness.pdf">Rising product digitalisation and losing trade competitiveness</a>.</i></b>
 </p><p>
-Jane Kelsey: <b><i><a href="http://ourworldisnotforsale.net/2017/Kelsey_E-commerce.pdf">E-commerce: the development implications of future proofing global trade rules for GAFA</a></i></b>
+Jane Kelsey: <b><i><a href="/2017/Kelsey_E-commerce.pdf">E-commerce: the development implications of future proofing global trade rules for GAFA</a></i></b>
 </p><p>
 <i><b>E-signatures and trade</b>:</i>
 English:
-<a href="http://ourworldisnotforsale.net/2017/Hill_E-signatures.pdf">pdf</a>.
+<a href="/2017/Hill_E-signatures.pdf">pdf</a>.
 
 <h3>Key Analysis: Agriculture</h3>
 </p><p>
-<a href="http://ourworldisnotforsale.net/2017/TWN_Agriculture.pdf"><i><b>Agriculture remains key</b></i></a>
+<a href="/2017/TWN_Agriculture.pdf"><i><b>Agriculture remains key</b></i></a>
 </p><p>
-<b><a href="../../agricultural-price-crisis.html"><i>WTO summit to ignore price crisis, agricultural dumping</i></a></b>
+<b><a href="o/agricultural-price-crisis.html"><i>WTO summit to ignore price crisis, agricultural dumping</i></a></b>
 </p><p>
-<b><i><a href="http://ourworldisnotforsale.net/2017/Wise_Food_security.pdf">WTO and Food Security: Biting the hand that feeds the poor</a></i></b>
+<b><i><a href="/2017/Wise_Food_security.pdf">WTO and Food Security: Biting the hand that feeds the poor</a></i></b>
 </p><p>
-<b><i><a href="http://ourworldisnotforsale.net/2017/DTE_Food_security.pdf">For India, the fight at WTO will be about food security</a></i></b>
+<b><i><a href="/2017/DTE_Food_security.pdf">For India, the fight at WTO will be about food security</a></i></b>
 </p>
 
         </div> <!-- /#content-area -->
