822291b5d732ef111ad89673c729fd0cde20a3ec
Norbert Bollow <nb@bollow.ch>
2021-03-30 10:51:47 +0200

    adding UNCTAD reports to digital page

--- theme-page/digital.php
+++ theme-page/digital.php
@@ -201,6 +201,21 @@ English:
         </div> <!-- /#content-area -->
       </div> <!-- /#content -->
     </div> <!-- /#main -->
+
+<h2>Documents from UNCTAD (UN Conference on Trade and Development)</h2>
+
+<a href="/2021/UNCTAD_ser-rp-2021d1_en.pdf">Research paper: <i>Joint Statement Initiative on E-Commerce (JSI):
+Economic and Fiscal Implications for the South</i></a>
+<p>
+Report: <i><a href="https://ourworldisnotforsale.net/2017/UNCTAD_competitiveness.pdf">Rising product digitalisation and losing trade competitiveness</a>.</i>
+<p>
+<i><a href="https://unctad.org/en/pages/PublicationWebflyer.aspx?publicationid=2227">Trade and Development Report 2018: Power, Platforms and The Free Trade Delusion</a>.</i>
+<p>
+<i><a href="https://ourworldisnotforsale.net/2019/UNCTAD_TDR2018_chapter_III_FR.pdf">Rapport sur le commerce et le développement 2018, chapitre III : le développement économique dans un monde numérique : perspectives, pièges et options politiques</a>.</i>
+<p>
+<i><a href="https://ourworldisnotforsale.net/2019/ser-rp-2019d1_en.pdf">Growing Trade in Electronic Transmissions: Implications for the South</a>.</i>
+
+
 <?php
 include 'include/foot_etc.php'
 ?>
