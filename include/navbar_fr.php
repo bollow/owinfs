9beb1f6f50224278331827b889805be484eebc25
Norbert Bollow <nb@bollow.ch>
2021-03-30 23:59:29 +0200

    French navbar update

--- include/navbar_fr.php
+++ include/navbar_fr.php
@@ -1,46 +1,25 @@
+<?php global $prepend_path; include 'include/hovermenu.php';?>
 <div id="navbar">
   <ul>
-    <li><a href="./">Accueil</a></li>
-    <li class="hovermenu"><a href="o/en/about">À propos</a>
-      <ul class="width2">
-        <li><a href="o/en/node/4">What is the WTO?</a></li>
-	<li><a href="o/en/node/2">Key positions and statements</a></li>
-	<li><a href="members">Membres</a></li>
-	<li><a href="o/en/node/2254">Becoming an OWINFS member</a></li>
-	<li><a href="o/en/node/3" title="WTO: Shrink or Sink! — A Critique of the WTO">Critique of the WTO</a></li>
-      </ul></li>
+    <li><a href="<?php echo $prepend_path; ?>./">Accueil</a></li>
+<?php global $this_aboutpage; hovermenu("data/aboutpages", $this_aboutpage, "width2", "À propos") ?>
     <!-- <li><a href="o/en/news">News</a></li> -->
-    <li class="hovermenu"><a href="o/en/themes">Thèmes</a>
-      <ul class="width3">
-        <li><a href="o/en/themes/3135">G-20</a></li>
-        <li><a href="o/en/themes/3084">Food &amp; Agriculture</a></li>
-        <li><a href="o/en/themes/3087">NAMA / ITA / EGs</a></li>
-        <li><a href="o/en/themes/3085">Services / GATS / TISA</a></li>
-        <li><a href="o/en/themes/3089">Trade &amp; The Climate Crisis</a></li>
-        <li><a href="o/en/themes/3090">Trade &amp; The Financial Crisis</a></li>
-        <li><a href="o/en/themes/3092">Transnational Corporations	</a></li>
-        <li><a href="o/en/themes/3086">Investments</a></li>
-        <li><a href="o/en/themes/3088">Intellectual Property/TRIPS</a></li>
-        <li><a href="o/en/themes/3094">Bilateral &amp; Regional Trade</a></li>
-        <li><a href="o/en/themes/3093">Alternatives</a></li>
-        <li><a href="o/en/themes/3105">Trade Facilitation</a></li>
-        <li><a href="o/en/themes/3083">WTO Process Issues</a></li>
-      </ul></li>
-    <li class="hovermenu"><a href="o/en/node/24715">Ministérielles passées</a>
+<?php global $this_theme; hovermenu("data/themes", $this_theme, "width3", "Thèmes") ?>
+    <li class="hovermenu"><a href="<?php echo $prepend_path; ?>o/en/node/24715">Ministérielles</a>
       <ul>
-        <li><a href="o/en/node/24715">11th Ministerial, Buenos Aires 2017</a></li>
-        <li><a href="o/en/ministerials/3260">10th Ministerial, Nairobi 2015</a></li>
-        <li><a href="o/en/ministerials/3166">9th WTO Ministerial, Bali 2013</a></li>
-        <li><a href="o/en/ministerials/3138">8th Ministerial, Geneva 2011</a></li>
-        <li><a href="o/en/ministerials/3106">7th WTO Ministerial Geneva 2009</a></li>
-        <li><a href="o/en/ministerials/3108">Mini Ministerial, Geneva 2008</a></li>
-        <li><a href="o/en/ministerials/3097">6th Ministerial, Hong Kong 2005 </a></li>
-        <li><a href="o/en/ministerials/3096">5th Ministerial, Cancùn 2003</a></li>
-        <li><a href="o/en/ministerials/3098">4th Ministerial, Doha 2001 </a></li>
-        <li><a href="o/en/ministerials/3109">3rd Ministerial, Seattle 1999</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/node/24715">11th Ministerial, Buenos Aires 2017</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3260">10th Ministerial, Nairobi 2015</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3166">9th WTO Ministerial, Bali 2013</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3138">8th Ministerial, Geneva 2011</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3106">7th WTO Ministerial Geneva 2009</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3108">Mini Ministerial, Geneva 2008</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3097">6th Ministerial, Hong Kong 2005 </a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3096">5th Ministerial, Cancùn 2003</a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3098">4th Ministerial, Doha 2001 </a></li>
+        <li><a href="<?php echo $prepend_path; ?>o/en/ministerials/3109">3rd Ministerial, Seattle 1999</a></li>
       </ul></li>
-    <li><a href="contact">Contact</a></li>
+    <li><a href="<?php echo $prepend_path; ?>contact">Contact</a></li>
     <li class="unbold"><a href="https://twitter.com/owinfs">Follow @owinfs</a></li>
-    <li><a href="https://www.facebook.com/owinfs/"><img src="f.png" width="14" height="14"></a></li>
+    <li><a href="https://www.facebook.com/owinfs/"><img src="<?php echo $prepend_path; ?>f.png" width="14" height="14"></a></li>
   </ul>
 </div>
