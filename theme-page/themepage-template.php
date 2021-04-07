2e709043b82d3e5640287cd10f529f909d30e568
Norbert Bollow <nb@bollow.ch>
2021-04-07 22:26:59 +0200

    new themepage-template

---
+++ theme-page/themepage-template.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+$html_title="Statements and papers regarding the  negotiations at WTO";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_theme="Fisheries";
+include 'include/sidebar_themes.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title"></h1>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+<h2>Statements and analysis from civil society</h2>
+
+<h2>Updates on the negotiations</h2>
+
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
