35a93cc51c8b09e6d58911b1ee9361f7a8124daa
Norbert Bollow <nb@bollow.ch>
2021-04-08 14:20:21 +0200

    Updated 'about us' page

---
+++ special-page/about.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?php
+$html_title="About OWINFS: Our World Is Not For Sale";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_aboutpage="About us";
+include 'include/sidebar_aboutpage.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title">About OWINFS: Our World Is Not For Sale</h1>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+
+    <p>The &ldquo;Our World is not for Sale&rdquo; (OWINFS) network is a loose grouping of organizations, activists and social movements worldwide fighting the current model of corporate globalization embodied in global trading system. OWINFS is committed to a sustainable, socially just, democratic and accountable multilateral trading system.</p>
+<p>OWINFS grew out of the international campaigns against the Multilateral Agreement on Investment (MAI) and the World Trade Organization (WTO). Linkages are also made between all the various pieces of the corporate-driven trade agenda, from the WTO to regional and bilateral agreements.</p>
+<p>While there are incredibly strong and diverse country-based-campaigns working on these issues around the world, it became clear that in order to contest for power against the multinational corporations and other proponents of the neo-liberal model that have been dictating global trade policy, there is a strong need to better coordinate and link these various initiatives. OWINFS supports and stimulates initiatives and collaboration among organizations and social movements in order to make our international strategies against WTO and trade liberalization more effective.</p>
+<p>OWINFS works to develop and link campaigns around the world toward the end of reshaping the corporate-dominated trade agenda to support human rights, environmental sustainability and democratic principles. OWINFS acts as a &ldquo;hub&rdquo; for social movements and NGOs working on globalization issues who are interested in sharing analysis and coordinating action efforts internationally. The active participation of OWINFS members is what drives our collective work forward. We coordinate efforts on conference calls and make decisions by consensus. A strength of the network is that individual movements and organizations can work together where it is strategic and helps advance their initiatives, and are free to dedicate as much or as little time to the network as makes sense for them in order to meet their objectives.</p>
+<p>Our efforts include:</p>
+<ul>
+<li>Convening regular international conference calls, meetings and e-mail discussions that provide movement leaders with the opportunity to share and develop ideas and strategy.</li>
+<li>Organizing delegations of social movement actors to Geneva, Switzerland where the WTO is headquartered, to advocate directly with negotiators and to provide them with critical analysis of the impacts of existing and proposed WTO policy.</li>
+<li>Organizing international press conferences, days of action and demonstrations in order to pressure government leaders and trade negotiators to roll-back harmful trade policy provisions, and to cease to craft more agreements that undermine the public interest.</li>
+</ul>
+
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
