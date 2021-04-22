b9ce4c0330721bd11f4293293b51139ab63ab98b
Norbert Bollow <nb@bollow.ch>
2021-04-22 19:16:30 +0200

    create page on the Seattle ministerial, because there was only a link leading to an empty page

---
+++ WTO-MC-page/MC3.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+$html_title="3rd Ministerial, Seattle 1999 | Our World Is Not For Sale";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_mc="3rd Ministerial, Seattle 1999";
+include 'include/sidebar_ministerials.php';
+include 'include/navbar.php';
+?>
+      <div id="content" class="singlecolumn">
+        <div id="content-header">
+          <h1 class="title">3rd Ministerial, Seattle 1999</h1>
+        </div> <!-- /#content-header -->
+
+        <div id="content-area">
+Looking back on the 1999 protests in Seattle:
+<a href="https://nwlaborpress.org/2019/11/looking-back-on-the-battle-in-seattle/">nwLaborPress.org article</a>
+(November 2019)
+<p>
+OWINFS’s founding statement is “<a href="no-new-round">No New Round – Turn Around</a>.” This is an international statement written in advance of the 1999 WTO Ministerial in Seattle — calling on WTO member-nations to reject expansion or a “new round”—and, instead, to roll-back harmful WTO provisions and policies. This statement was signed by nearly 1500 organizations in the run-up to the Ministerial.
+<p>
+Our second statement was “<a href="shrink-or-sink">Our World is not for Sale: WTO – Shrink or Sink!</a>” This international “sign-on” statement, prepared after the Seattle Ministerial, lays out eleven transformational demands aimed at the WTO. Active OWINFS members have signed onto the statement.
+
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
