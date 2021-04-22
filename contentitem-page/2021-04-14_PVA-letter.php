99ef4667903ae750b44d3485d043d3b81271c325
Norbert Bollow <nb@bollow.ch>
2021-04-22 13:30:11 +0200

    TRIPS page updates as per Deborah’s email of 2021-04-16

---
+++ contentitem-page/2021-04-14_PVA-letter.php
@@ -0,0 +1,87 @@
+#!/usr/bin/php
+<?php
+$html_title="Former Heads of State and Nobel Laureates Call on President Biden To Waive Intellectual Property Rules for COVID Vaccines";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$sidebar_title="About us";
+include 'include/sidebar_about.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+        <div id="content-header">
+          <h1 class="title">Former heads of state and Nobel laureates call on President Biden to waive intellectual property rules for COVID vaccines</h1>
+	  <h4></h4>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+14 April 2021
+<p>
+More than 170 former heads of state and government and Nobel laureates, including former Prime Minister of the United Kingdom Gordon Brown, former President of Colombia Juan Manuel Santos, former President of Liberia Ellen Johnson Sirleaf, former President of France François Hollande and Nobel Laureates Professor Joseph Stiglitz and Professor Francoise Barre-Sinoussi, today called on President Biden to support a waiver of intellectual property rules for COVID-19 vaccines and pursue a people’s vaccine to end the pandemic in an open letter today. A waiver of intellectual property rules would allow for a scale up in manufacturing in the U.S. and around the world, overcoming artificial supply constraints.
+<p>
+The former world leaders and Nobel Laureates encourage President Biden to take the urgent action only he can and “let this moment be remembered in history as the time we chose to put the collective right to safety for all ahead of the commercial monopolies of the few.”
+<p>
+The letter specifically asks President Biden to support a proposal from the South African and Indian governments at the World Trade Organization (WTO) to temporarily waive intellectual property rules related to COVID-19 vaccines and treatments. At the current pace of vaccine production, most poor nations will be left waiting until at least 2024 to achieve mass COVID-19 immunization.
+
+ <p>
+<b>Gordon Brown, former Prime Minister of the United Kingdom said:</b>
+<p>
+<i>“President Biden has said that no one is safe until everyone is safe, and now with the G7 ahead there is an unparalleled opportunity to provide the leadership that only the U.S. can provide and that hastens an end to the pandemic for the world.”</i>
+<p>
+<i>“An urgent temporary waiver of intellectual property rules at the World Trade Organization would help us ramp up global supply of vaccines together with a global multi-year burden sharing plan to finance vaccines for the poorest countries”.</i>
+<p>
+<i>“This would be in the strategic interests of the U.S., and of every country on the planet".</i>
+<p>
+<b>Joseph Stiglitz, Nobel Economics Prize Laureate, said:</b>
+<p>
+<i>“While the U.S. has made enormous progress in vaccinating its own population, thanks to the efforts of the Biden administration, that alone is unfortunately not enough”.</i>
+<p>
+<i>“New mutations of the virus will continue to cost lives and upend our interconnected global economy until everyone, everywhere has access to a safe and effective vaccine. Intellectual property is the utmost artificial barrier to global vaccine supply. We as a nation must lead with our allies to back the South Africa and India waiver at the WTO, insist on technology transfer, and strategically invest in production”.</i>
+<p>
+<b>François Hollande, former President of France, said:</b>
+<p>
+<i>“The extreme inequality in access to vaccines around the world creates an unbearable political and moral situation. It is above all sanitary and economic nonsense as we are all concerned. That the Biden administration is considering waiving barriers related to intellectual property rules offers hope for the international community. If the United States supports the lifting of patents, Europe will have to take its responsibilities. In the face of this devastating pandemic, world leaders must prioritize the public interest and international solidarity”.</i>
+<p>
+Other signatories include Mary Robinson, the former President of Ireland, Fernando Henrique Cardoso, the former President of Brazil, and Helen Clark, the former Prime Minister of New Zealand, together with over 60 other former heads of state and heads of government that span every continent.
+<p>
+The leaders also called for the intellectual property waiver to be accompanied by the open sharing of vaccine know-how and technology, and by coordinated and strategic global investment in research, development, and manufacturing capacity, especially in developing countries, underscoring that threats to public health are global and require global solidarity-based solutions.       
+<p>
+These actions would expand global manufacturing capacity, unhindered by industry monopolies that are driving the dire supply shortages blocking vaccine access. The resulting vaccine inequality, the leaders warned, means that the U.S. economy already risks losing $1.3 trillion in GDP this year, and if the virus is left to roam the world, the increased risk of new viral variants means even vaccinated people in the U.S. could be unprotected once more. 
+
+ 
+<p>
+The letter, which was coordinated by the People’s Vaccine Alliance, a coalition of more than 50 organizations including Club de Madrid, Public Citizen, Oxfam and UNAIDS, warned that at the current global immunization rate, it was likely that only 10 percent of people in the majority of poor countries will be vaccinated in the next year. 
+<p>
+<b>Françoise Barré-Sinoussi, Nobel Prize in Physiology or Medicine Laureate said:</b>
+<p>
+<i>“We will not end today’s global pandemic until rich countries – most especially the United States – stop blocking the ability of countries around the world to mass produce safe and effective vaccines”.</i>
+<p>
+<i>“Global health is on the line. History is watching. I, with my fellow laureates and scientists across the globe, urge President Biden to do the right thing and to support the TRIPS waiver, insist on pharmaceutical corporations to share vaccine technologies with the world, and strategically invest in distributed production”.</i>
+<p>
+<b>Muhammad Yunus, Nobel Peace Prize Laureate said:</b>
+<p>
+“Big pharmaceutical companies are setting the terms of the end of today's pandemic – and the cost of allowing senseless monopolies is only more death and more people being pushed into poverty”.
+<p>
+“Only government action - not philanthropy, and not the private sector - can solve today’s unprecedented crisis. We together urge President Biden to stand on the right side of history – and ensure a vaccine is a global common good, free of intellectual property protections". 
+<p>
+&nbsp;
+<p>
+<hr>
+<p>
+The full letter and list of signers can be found <a href="https://peoplesvaccinealliance.medium.com/open-letter-former-heads-of-state-and-nobel-laureates-call-on-president-biden-to-waive-e0589edd5704">here</a>.
+<p>
+The letter was coordinated by the People’s Vaccine Alliance, a coalition of more than 50 organizations including Club de Madrid, the Yunus Centre, Oxfam, Public Citizen, Health GAP, UNAIDS, Physicians for Human Rights, the Nizami Ganjavi International Center, Global Justice Now and Avaaz.
+
+ 
+
+
+
+
+</div> <!-- /#content-area -->
+</div> <!-- /#content -->
+</div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
