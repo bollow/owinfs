46addc6de040ce1f17cdb77b6648c93473758588
Norbert Bollow <nb@bollow.ch>
2021-04-29 11:03:37 +0200

    add dates to documents on UNCTAD page

--- theme-page/UNCTAD.php
+++ theme-page/UNCTAD.php
@@ -19,26 +19,33 @@ include 'include/navbar.php';
 <h2>Towards UNCTAD XV</h2>
 UNCTAD XV quadrennial conference is scheduled to take place virtually on October 3-8, 2021.
 <p>
-<i><a href="2021-04-06_CSO-letter_UNCTAD">Letter from CSOs to UNCTAD</a></i>:
+<i><a href="2021-04-06_CSO-letter_UNCTAD">Letter from CSOs to UNCTAD</a></i>, April 6, 2021:
 <a href="/2021/CSOs_to_UNCTAD_2021-04-06.pdf">pdf</a>
 <p>
-<a href="/2021/TWN_UNCTAD_issues.pdf"><i>TWN Brief on key issues for developing countries while negotiations continue on UNCTAD XV</i></a>
+<a href="/2021/TWN_UNCTAD_issues.pdf"><i>TWN Brief on key issues for developing countries while negotiations continue on UNCTAD XV</i></a>, March 2021.
 
 <h2>Documents from UNCTAD</h2>
 <a href="/2021/ser-rp-2021d8_en.pdf">Reforming the international trading system for recovery, resilience and inclusive development</a>,
 UNCTAD Research Paper No. 65, April 2021.
 <p>
 <a href="/2021/UNCTAD_ser-rp-2021d1_en.pdf">Research paper: <i>Joint Statement Initiative on E-Commerce (JSI):
-Economic and Fiscal Implications for the South</i></a>
-<p>
-Report: <i><a href="https://ourworldisnotforsale.net/2017/UNCTAD_competitiveness.pdf">Rising product digitalisation and losing trade competitiveness</a>.</i>
-<p>
-<i><a href="https://unctad.org/en/pages/PublicationWebflyer.aspx?publicationid=2227">Trade and Development Report 2018: Power, Platforms and The Free Trade Delusion</a>.</i>
-<p>
-<i><a href="https://ourworldisnotforsale.net/2019/UNCTAD_TDR2018_chapter_III_FR.pdf">Rapport sur le commerce et le développement 2018, chapitre III : le développement économique dans un monde numérique : perspectives, pièges et options politiques</a>.</i>
+Economic and Fiscal Implications for the South</i></a>,
+February 2021.
 <p>
 <i><a href="https://ourworldisnotforsale.net/2019/ser-rp-2019d1_en.pdf">Growing Trade in Electronic Transmissions: Implications for the South</a>.</i>
-
+UNCTAD Research Paper No. 29, February 2019.
+<p>
+UNCTAD <i>Trade and Development Report 2018: Power, Platforms and The Free Trade Delusion:
+<a href="https://unctad.org/en/pages/PublicationWebflyer.aspx?publicationid=2227">English</a>,
+<a href="https://unctad.org/es/PublicationsLibrary/tdr2018_es.pdf">Spanish</a>,
+<a href="https://unctad.org/fr/PublicationsLibrary/tdr2018_fr.pdf">French</a>.</i>
+(September 2018)
+<br>
+→ Chapter III: <i>Economic Development in a Digital World: Prospects, Pitfalls and Policy Options</i>:
+      <a href="https://unctad.org/en/PublicationChapters/tdr2018ch3_en.pdf">English</a>,
+      <a href="/2019/UNCTAD_TDR2018_chapter_III_FR.pdf">French</a>.
+<p>
+Report: <i><a href="https://ourworldisnotforsale.net/2017/UNCTAD_competitiveness.pdf">Rising product digitalisation and losing trade competitiveness</a></i>, November 2017.
 
         </div> <!-- /#content-area -->
       </div> <!-- /#content -->
