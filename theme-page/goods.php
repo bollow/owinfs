00f2b45c1ae5a04695a9e41a8f48fa2d2cc1cd53
Norbert Bollow <nb@bollow.ch>
2021-04-29 13:49:51 +0200

    new 'Trade in Goods' theme page

---
+++ theme-page/goods.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+$html_title="Trade in Goods";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_theme="Trade in Goods";
+include 'include/sidebar_themes.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title">Trade in Goods</h1>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+Setting rules for trade in goods is the original mandate of the General Agreement on Tariffs and Trade (GATT), the predecessor of the WTO. Goods negotiations include a legal recognition of Less Than Full Reciprocity (LTFR) which acknowledges the differing levels of development amongst WTO members. 
+<p>
+Talks on rules governing trade in goods in the WTO are predominantly located under the Non-Agricultural Market Access (NAMA) negotiations. These were part of the original Doha negotiations launched in 2001. Trade unions from developing countries fought vociferously against unfair tariff cuts that would have decimated jobs and workers’ collective bargaining power. They were successful in derailing an agreement to conclude the Doha Round in 2008. 
+<p>
+In addition, proponents of tariff cuts have also conducted talks towards two “plurilateral” agreements in the WTO. 
+<p>
+An agreement for zero tariffs on goods classified as “information technology” was concluded in 1996. The Information Technology Agreement (ITA) was expanded and updated in December 2015. OWINFS members opposed this expansion to the ITA II. If developing countries believe that lowering their tariffs would be good for their development, they can do so unilaterally. 
+<p>
+Another proposed agreement for zero tariffs on goods classified as “environmental” (which included goods which both help but also harm the environment). Talks towards an Environmental Goods Agreement (EGA) are currently abandoned. 
+<p>
+Rather than using trade to increase employment and prosperity, the WTO rules focus on reducing tariffs and regulation and other policies which increase the bargaining power of capital at the expense of labor. This forces workers into unfair competition, resulting in a lack of minimum living wage and labor protections, increased job precariousness, the exploitation of women, and unsafe working environments. Capital’s increased freedom of movement and WTO-led globalization has triggered a race to the bottom where countries and special export zones compete in repressing labor rights in order to attract investment. Global supply chains, the dominant production model today, are beset with slavery, child labor, and all forms of labor exploitation that affect women workers disproportionally. Instead, OWINFS members argue that we need a global trade system that focuses on increasing decent job opportunities and living standards of people who work.
+<ul>
+  <li>Any current or future agreement must focus on using trade to promote and expand Decent Work with a new social contract and a labor protection floor as called for in the ILO Centenary Declaration, and protecting policy space of countries to pursue shared prosperity through full employment, through structural transformation and industrialization and through higher wages and social dialogue.
+  <li>The negotiations towards an Environmental Goods Agreement should abandoned.
+  <li>In any future negotiations in Non-Agricultural Market Access (NAMA), the Swiss formula should be abandoned; talks must be based on the mandate of Less Than Full Reciprocity; discussions on sectorals must be voluntary; manufacturing jobs should not be “traded off” for agricultural jobs, and there should be no anti-concentration clause, as countries must maintain the flexibility to protect vulnerable and labor-intensive sectors.
+  <li>In addition, we call for Social Impact Assessments including on inequality, the quality of employment and on job creation with regards to current, and any potential future changes to WTO policies and negotiations, and the implementation of changes to existing rules that are necessary to ensure a multilateral trade system that promotes full employment and decent work.
+</ul>
+For civil society statements and updates on negotiations before the 2015 Ministerial, please see
+<a href="/o/en/themes/3087">here</a>. 
+
+
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
