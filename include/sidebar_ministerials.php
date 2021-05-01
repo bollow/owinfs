6529362d8c95dd7cbad0aaec5f1e26cd71aca2d4
Norbert Bollow <nb@bollow.ch>
2021-05-01 10:03:31 +0200

    'about us' fixes

--- include/sidebar_ministerials.php
+++ include/sidebar_ministerials.php
@@ -32,8 +32,8 @@ while(!feof($in)) {
         <h2 class="title">&nbsp;</h2>
         <img src="logo_owinfs_red.png" alt="Our World Is Not For Sale logo"
              width="230" height="225">
-        <p>The “Our World is not for Sale” (OWINFS) network is a loose grouping of organizations,
-        activists and social movements worldwide fighting the current model of corporate
+        <p>The “Our World Is Not For Sale” (OWINFS) network is a loose grouping of organizations
+        and social movements worldwide fighting the current model of corporate
         globalization embodied in global trading system. OWINFS is committed to a sustainable,
         socially just, democratic and accountable multilateral trading system.</p>
 	<p><a href="statement-political-unity">Statement of political unity</a></p>
--- include/sidebar_themes.php
+++ include/sidebar_themes.php
@@ -35,8 +35,8 @@ while(!feof($in)) {
        <img src="<?php echo $prepend_path; ?>logo_owinfs_red.png" alt="Our World Is Not For Sale logo"
              width="230" height="225">
         <h2 class="title">About us</h2>
-        <p>The “Our World is not for Sale” (OWINFS) network is a loose grouping of organizations,
-        activists and social movements worldwide fighting the current model of corporate
+        <p>The “Our World Is Not For Sale” (OWINFS) network is a loose grouping of organizations
+        and social movements worldwide fighting the current model of corporate
         globalization embodied in global trading system. OWINFS is committed to a sustainable,
         socially just, democratic and accountable multilateral trading system.</p>
 	<p><a href="statement-political-unity">Statement of political unity</a></p>
