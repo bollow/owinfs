615b064bd5672ed5e79e86b6921c2f5c815033d1
Norbert Bollow <nb@bollow.ch>
2021-05-01 16:06:14 +0200

    added many links to agriculture theme page

--- theme-page/agriculture.php
+++ theme-page/agriculture.php
@@ -34,25 +34,140 @@ Countries should be able to use tariffs to protect domestic food markets. In any
 Subsidies that the US and the EU provide to cotton producers enrich a few thousand there, but have unfairly decimated production of hundreds of thousands of cotton farmers in Africa. <b>It is unfortunate that WTO members did not decide to significantly reduce or eliminate developed countries’ domestic supports for cotton at <a href="MC11">MC11</a></b>. 
 
 <h2>Civil Society Statements and Analysis</h2>
+
+<i>Rebuilding the WTO for a sustainable global development</i>:
+<a href="https://www.sol-asso.fr/wp-content/uploads/2020/01/Rebuilding-the-WTO-for-a-sustainable-global-development-J.-Berthelot-July-12-2020.pdf">English</a>,
+<a href="https://www.sol-asso.fr/wp-content/uploads/2020/01/Reconstruire-lOMC-pour-un-d%C3%A9veloppement-plan%C3%A9taire-durable-J.-Berthelot-12-juillet-2020.pdf">French</a>.
+(Jacques Berthelot, SOL France, July 2020)
+<p>
+<i>Agreement on Agriculture and Food: Preliminary proposal</i>:
+<a href="https://www.sol-asso.fr/wp-content/uploads/2019/01/Agreement-on-Agriculture-and-Food-AoAF-SOLs-proposal-of-22-January-2019.pdf">English</a>,
+<a href="https://www.sol-asso.fr/wp-content/uploads/2019/01/Accord-sur-lagriculture-et-lalimentation-AsAA-de-lOMC-SOL-22-01-2019.pdf">French</a>.
+(Jacques Berthelot, SOL France, January 2019)
+
+<p>
 <i>Inequitable WTO rules in agriculture continue to impact farmers in the developing world</i>:
 <a href="https://ourworldisnotforsale.net/2018/ALAI_farmers.pdf">English</a>, 
 <a href="https://ourworldisnotforsale.net/2018/ALAI_normas_agrícolas.pdf">Spanish</a>
-(April 2018)
+(Ranja Sengupta, ALAI, April 2018)
 <p>
 <a href="/2017/TWN_Agriculture.pdf"><i>Agriculture remains key</i></a>
-(22 November 2017)
+(Ranja Sengupta, 22 November 2017)
 <p>
 <a href="o/agricultural-price-crisis.html"><i>WTO summit to ignore price crisis, agricultural dumping</i></a>
-(10 December 2017)
+(Timothy Wise, 10 December 2017)
 <p>
 <i><a href="/2017/Wise_Food_security.pdf">WTO and Food Security: Biting the hand that feeds the poor</a></i>
-(December 2017)
+(Timothy Wise, December 2017)
 <p>
 <i><a href="/2017/DTE_Food_security.pdf">For India, the fight at WTO will be about food security</a></i>
-(9 December 2017)
+(Sachin Kumar Jain, 9 December 2017)
+
+<h2>Other Key Analysis</h2>
+<p>
+ <i><a href="http://wtocentre.iift.ac.in/workingpaper/WorkingPaper64.pdf">Disciplining Trade-Distorting Support to Cotton in the US: An Unfinished Agenda in WTO Negotiations</a></i>. (Centre for WTO Studies, India, January 2021) 
+<p>
+ <i><a href="http://wtocentre.iift.ac.in/workingpaper/Working%20Paper%2063.pdf">A Quantitative Analysis of Proposals on Domestic Support in WTO Agriculture Negotiations: Need for Reaffirming the Development Agenda</a></i>. (Centre for WTO Studies, India, September 2020) 
+<p>
+ <i><a href="http://wtocentre.iift.ac.in/workingpaper/Working%20Paper%2062.pdf">Demystifying Blue Box Support to Agriculture Under the WTO: Implications for Developing Countries</a></i>. (Centre for WTO Studies, India, September 2020) 
+<p>
+ <i><a href="http://wtocentre.iift.ac.in/workingpaper/WorkingPaper59.pdf">Special Safeguard Mechanism for Agriculture: Implications for Developing Members at the WTO</a></i>. (Centre for WTO Studies, India, August 2020) 
+
+<h2>Articles &amp; Updates on the Negotiations</h2>
+
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2021/ti210208.htm">Agriculture talks in disarray over assault on development, S&amp;DT issues</a></i>, by D. Ravi Kanth. (10 February 2021)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti201223.htm">Proponents of proposal on export restrictions suffer major setback</a></i>, by D. Ravi Kanth. (22 December 2020)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti201221.htm">Questions raised on exempting WFP purchases from export restrictions</a></i>, by D. Ravi Kanth. (21 December 2020)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti200623.htm">North provided billions in farm support during COVID-19</a></i>, by D. Ravi Kanth. (27 June 2020)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti200406.htm">India invokes MC9 decision for food security public stockholding</a></i>, by D. Ravi Kanth. (7 April 2020)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti200210.htm">Chair suggests incremental outcomes on SSM &amp; PSH at MC12</a></i>, by D. Ravi Kanth. (21 February 2020)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2019/ti191027.htm">US escalating efforts for market access outcomes in agriculture</a></i>, by D. Ravi Kanth. (31 October 2019)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2019/ti190725.htm">Agriculture: US not ready to negotiate domestic support issues</a></i>, by Kanaga Raja. (22 July 2019)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2019/ti190307.htm">Major North countries targeting South over agriculture</a></i>, by D. Ravi Kanth. (15 March 2019)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti181003.htm">Developed countries turn their backs on permanent solution for PSH</a></i>, by D. Ravi Kanth. (9 October 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti180713.htm">G33 for policy instruments to improve food security</a></i>, by D. Ravi Kanth. (16 July 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti180707.htm">China-India renew battle to end US-EU agri-support policies</a></i>, by D. Ravi Kanth. (9 July 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti180520.htm">US-Australia-Brazil try hijacking Doha agri-talks from core mandates</a></i>, by D. Ravi Kanth. (30 May 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti180513.htm">US aiming to make flawed and skewed UR agri-disciplines permanent</a></i>, by D. Ravi Kanth. (16 May 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171231.htm">CSOs disappointed over MC11 failure to deliver on PSH, development</a></i>, by Kanaga Raja. (15 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171223.htm">CSOs deeply disappointed with MC11 draft agriculture text of 12 December</a></i>. (14 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171221.htm">US NO to improving PSH peace clause, MC11 inching to collapse</a></i>, by D. Ravi Kanth. (13 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171219.htm">G33 call for strong outcome on PSH and SSM</a></i>, by D. Ravi Kanth. (12 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171118.htm">Agriculture: Talks in final stretch with limited time before MC11, says Chair</a></i>, by Kanaga Raja. (20 November 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171101.htm">No convergence on PSH or domestic support reduction</a></i>, by D. Ravi Kanth. (1 November 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171020.htm">Moves for permanent solution for PSH for MC11 ministers</a></i>, by D. Ravi Kanth. (26 October 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171016.htm">US NO to Cotton-four, but China-India welcome proposal</a></i>, by D. Ravi Kanth. (20 October 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170913.htm">&quot;Will not pay for mandated permanent solution for PSH&quot;, insist G33</a></i>, by D. Ravi Kanth. (21 September 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170905.htm">Agriculture: No credible outcomes likely at MC11 on domestic support</a></i>, by D. Ravi Kanth. (14 September 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170711.htm">South nations for ending AMS, cuts in &quot;green box&quot; in agriculture</a></i>, by D. Ravi Kanth. (17 July 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170703.htm">EU and Brazil oppose end to 'green box' cotton payments</a></i>, by D. Ravi Kanth. (11 July 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170601.htm">Permanent solution for PSH seems unlikely at MC11</a></i>, by D. Ravi Kanth. (2 June 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170415.htm">G33 insist on unambiguous outcomes at MC11 on food security</a></i>, by D. Ravi Kanth. (28 April 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170319.htm">PSH must cover all DCs, not country-specific</a></i>, by D. Ravi Kanth. (23 March 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170103.htm">LDCs push for concrete outcomes at MC11 on agri domestic support</a></i>, by D. Ravi Kanth. (23 January 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti161205.htm">Diminishing power of small-scale farmers in cocoa GVCs</a></i>, by Kanaga Raja. (9 December 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti161119.htm">Cotton producers call for an outcome on cotton at MC11</a></i>, by Kanaga Raja. (30 November 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti161116.htm">Agriculture: Proposals tabled on domestic support, market access</a></i>, by Kanaga Raja. (25 November 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160722.htm">US, EU and Australia want &quot;hefty payment&quot; for PSH and SSG</a></i>, by Ravi Kanth. (25 July 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160705.htm">Deliver on &quot;Cotton issue&quot; before e-commerce talks, WTO told</a></i>, by D. Ravi Kanth. (12 July 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160512.htm">Developing countries stress on importance of SSM</a></i>, by Kanaga Raja. (17 May 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160508.htm">Hollowness of Nairobi export competition accord exposed</a></i>, by D. Ravi Kanth. (13 May 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160403.htm">Time for Developing Countries to sue US, EU over AoA violations</a></i>, by Jacques Berthelot. (7 April 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160307.htm">SSM, public stockholding priority issues, say South</a></i>, by Kanaga Raja. (14 March 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151220.htm">Nairobi green signal for continued US trade-distorting agri-exports</a></i>, by D. Ravi Kanth. (23 December 2015)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151216.htm">Civil society expresses shock at draft agriculture text</a></i>, by Ranja Sengupta. (18 December 2015)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151211.htm">Rights expert calls for concrete outcomes on food security</a></i>, by Kanaga Raja. (17 December 2015)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151208.htm">China, India, Indonesia &amp; SA press for outcome on food security, SSM</a></i>, by D. Ravi Kanth. (15 December 2015)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151206.htm">CSOs stress on development, food security, LDC issues at MC10</a></i>, by Kanaga Raja. (15 December 2015)  
+<p>
+    
 
 <h2><a href="o/en/themes/3084">Previous work</a></h2>
-For previous work, <a href="o/en/themes/3084">see here</a>.
+OWINFS has campaigned for the above policy changes to the WTO since our founding. For our work before 2017, please <a href="o/en/themes/3084">see here</a>.
 
 <p>&nbsp;<p>
 
