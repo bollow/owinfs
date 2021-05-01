2f4ea763f647386527286e31b88aef59039331c8
Norbert Bollow <nb@bollow.ch>
2021-05-01 14:42:04 +0200

    new content for the 'services' page

--- theme-page/services.php
+++ theme-page/services.php
@@ -15,24 +15,92 @@ include 'include/navbar.php';
         <div id="content-header">
           <h1 class="title">Services / GATS</h1>
         </div> <!-- /#content-header -->
-        
+<p>
+The General Agreement on Trade in Services (GATS) is the agreement within the WTO that sets forth rules for how countries can regulate services, and gives transnational services suppliers rights not only to operate, but also rights in the way that they operate, in foreign countries. Countries have agreed to “bind” their services sectors to WTO rules in their Schedules in the GATS. This agreement was part of the founding of the WTO due to the lobbying of Big Finance and other transnational corporations (TNCs) in the services sector. The majority of developing countries have long opposed the expansion of the GATS, and talks faltered as the Doha round stalled. 
+<p>
+Proponents then launched talks towards a services-only pact, the Trade in Services Agreement (TiSA). OWINFS members, together with Public Services International, conducted a grassroots and advocacy campaign for four years against the TiSA. We were successful in December 2016 when the talks collapsed. 
+<p>
+Now, a dangerous corporate agenda is behind a new effort to have new rules limiting domestic regulation of services, within the General Agreement on Trade in Services (GATS) of the WTO. “Domestic Regulation” (DR) rules would restrict the types of rules that governments can make even if the rules are applied the same to foreign and domestic companies. They would even apply to domestic (non-traded) services (like construction) as well as services that are traded (like air travel). The proposed rules would apply to private companies as well as to public services not covered by the narrow public services exception. 
+<p>
+While proponents argue that the “right to regulate” would still be safeguarded, the WTO has ruled in the past that “Members’ regulatory sovereignty is an essential pillar of the progressive liberalization of trade in services, but this sovereignty ends whenever rights of other Members under the GATS are impaired.” Thus the “right to regulate” ends where the WTO rules begin. None of the documents on the negotiations have been made public, yet the text has been finalized and Members are intending to finalize the agreement before or during the upcoming 12th Ministerial Conference (MC12) in Geneva in December, 2021. 
+<p>
+In addition, some countries are seeking to restart the negotiations to expand the scope and coverage of the GATS in the WTO. This would involve countries agreeing to subject even more services sectors to the disciplines of the WTO, which would further privilege foreign transnational services providers at the expense of domestic services. Services sectors currently targeted include environmental (engineering, architecture, construction (water related), distribution, data processing, consulting); tourism and related; agriculture related (services incidental to agriculture such as wholesale trade in live animals, sale on contract of food products, wholesale trade in food and beverage, food retailing); and logistics. 
+This agenda also overlaps with the digital trade negotiations, as many services sectors targeted relate to the information and communications sector, and rules on cross-border data transfer are also included in the GATS. 
+<p>
+One of the primary concerns of civil society with regards to the proposed DR rules, like those of Investment Facilitation, is the “Transparency” provisions which would give foreign TNCs greater input and oversight over domestic policymaking processes at the expense of public interest concerns of citizens.  
+<p>
+OWINFS members have long maintained core demands with regards to services in the WTO: 
+<ul class="tail">
+  <li>Countries should not take additional commitments to “discipline” their services sectors, through expanded GATS negotiations, Domestic Regulation rules, or through digital trade talks. 
+  <li>Current and future public services in particular must be excluded from any existing or future agreements that include services with a broad and authentic carve-out. This must include services that may need to be re-municipalized in future.
+  <li>In addition, a fundamental assessment of the implications of the GATS on ensuring proper regulatory oversight of services and ensuring access to quality public services should be undertaken by member states and the WTO. Any constraints on achieving the Sustainable Development Goals (SDGs) with regards to good health and well-being, inclusive and equitable quality education, clean water and sanitation, affordable and clean energy, and other services should be removed without challenge from the WTO rulebook.
+</ul>
+
 <h2>Current Civil Society Statements and Analysis</h2>
-<a href="/2019/TWN_103_services.pdf"><i>Disciplining non-discriminatory domestic regulations in the services sectors – Another plurilateral track at the WTO</i></a>
+<i><a href="https://twn.my/title/tnd/td44.htm">Plurilateral Initiatives and their Interaction with WTO Rules</a></i>. (Kinda Mohamadieh, TWN, 2020)
 <p>
-<i>“Domestic regulation” rules in the WTO:</i>
+<i><a href="https://www.twn.my/title2/FTAs/Services/Full%20report%20for%20TD%20series_FORMAT_Ver6-FIN-09012020.pdf">Understanding the European Union’s understanding on computer and related services</a></i>. (Jane Kelsey, TWN, September 2020)
+<p>
+Briefing paper: <a href="/2019/TWN_103_services.pdf"><i>Disciplining non-discriminatory domestic regulations in the services sectors – Another plurilateral track at the WTO</i></a>. (Kinda Mohamadieh, TWN, October 2019)
+<p>
+Two-page summary: <i>“Domestic regulation” rules in the WTO:</i>
 English:
-<a href="https://ourworldisnotforsale.net/2017/Domestic_Regulation.pdf">pdf</a>,
-<a href="https://ourworldisnotforsale.net/2017/Domestic_Regulation.docx">docx</a>.
+<a href="/2017/Domestic_Regulation.pdf">pdf</a>,
+<a href="/2017/Domestic_Regulation.docx">docx</a>.
 Spanish:
-<a href="https://ourworldisnotforsale.net/2017/Domestic_Regulation_ES.pdf">pdf</a>,
-<a href="https://ourworldisnotforsale.net/2017/Domestic_Regulation_ES.doc">doc</a>.
+<a href="/2017/Domestic_Regulation_ES.pdf">pdf</a>,
+<a href="/2017/Domestic_Regulation_ES.doc">doc</a>.
 <p>
-Session at the 2019 WTO Public Forum: <a href="/2019_WTO_PF_111">#111. What would services as the next trade frontier look like?</a>
+Comprehensive analysis: <i><a href="/2017/TWN_DRD_analysis.pdf">Analyses of Domestic Regulation Disciplines</a></i>. (Sanya Reid Smith, TWN, December 2017)
 <p>
-Session at the 2019 WTO Public Forum: <a href="/2019_WTO_PF_119">#119. Why are computer and related services important?</a>
+Session at the 2019 WTO Public Forum: <a href="/2019_WTO_PF_111">#111. <i>What would services as the next trade frontier look like?</i></a> (11 October 2019)
+<p>
+Session at the 2019 WTO Public Forum: <a href="/2019_WTO_PF_119">#119. <i>Why are computer and related services important?</i></a> (11 October 2019)
+<p>
+Book: <i><a href="https://movendi.ngo/wp-content/uploads/2015/03/POLITICAL-ECONOMY-Serving-whose-interests-the-political-economy-of-international-trade-in-service.pdf">Serving Whose Interests? The political economy of trade in services agreements.</i></a>
+(Jane Kelsey, Routledge Cavendish, 2008)
+
+<h2>Leaked Texts</h2>
+<i><a href="https://bilaterals.org/?wto-plurilateral-services-domestic-43658">WTO plurilateral services domestic regulation disciplines draft schedules</a></i>.
+These are the services sectors they are proposing the new rules would apply to.
+(10 February 2021)
+<p>
+<i><a href="https://bilaterals.org/?wto-plurilateral-services-domestic-43646">WTO plurilateral services domestic regulation disciplines - Chair’s summary of meeting held on 10 December 2020</a></i>. (9 February 2021)
+<p>
+<i><a href="https://bilaterals.org/?wto-plurilateral-services-domestic">WTO plurilateral services domestic regulation disciplines draft text as of 18 December 2020</a></i>.
+
+<h2>Some Articles &amp; Updates on the Negotiations</h2>
+ 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2020/ti200805.htm">Renewed JSI efforts to extend e-com duties moratorium to services</a></i>, by D. Ravi Kanth. (6 August 2020) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2020/ti200601.htm">US to target countries that impose digital services taxes</a></i>, by D. Ravi Kanth. (4 June 2020) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2020/ti200316.htm">WTO &quot;Friends of Services&quot; for permanent moratorium on E-transmissions</a></i>, by D. Ravi Kanth. (28 March 2020) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2019/ti190721.htm">US launches probe into France's proposed digital services tax</a></i>, by D. Ravi Kanth. (15 July 2019) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2019/ti190419.htm">EU, Australia table draft MC12 statement on domestic regulation</a></i>, by D. Ravi Kanth. (25 April 2019) 
+<p>
+ <i><a href="https://www.twn.my/title2/FTAs/Services/Full%20report%20for%20TD%20series_FORMAT_Ver6-FIN-09012020.pdf">Understanding the European Union’s Understanding on Computer and Related Services</a></i>, by Professor Jane Kelsey  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171109.htm">No outcome likely at MC11 on domestic regulation in services</a></i>, by D. Ravi Kanth. (10 November 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171022.htm">Outcome unlikely on domestic regulation in services at MC11</a></i>, by D. Ravi Kanth. (26 October 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171003.htm">India, Africa resist IC moves to finalize outcomes in GATS DR at MC11</a></i>, by D. Ravi Kanth. (9 October 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170512.htm">Efforts to import parts of failed TiSA into services DR thwarted</a></i>, by D. Ravi Kanth. (12 May 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti170204.htm">Green room process on services domestic regulation</a></i>, by D. Ravi Kanth. (6 February 2017)  
+<p>
+  
+
+For more articles and updates on the negotiations see
+<a href="https://www.twn.my/services_negotiations.htm">here</a>.
 
 <h2><a href="o/en/themes/3085">Previous work</a></h2>
-For previous work, <a href="o/en/themes/3085">see here</a>.
+OWINFS members, together with Public Services International, conducted a grassroots and advocacy campaign for four years against the TiSA, which was successful in December 2016 when the talks collapsed. OWINFS has also conducted many years of campaigning against the expansion of the GATS. This work can be seen <a href="o/en/themes/3085">here</a>.
 
 <p>&nbsp;<p>
 
