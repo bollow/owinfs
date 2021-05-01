fd9ebf7a5ec10410dd4a82aac1a2449b7deb9ed7
Norbert Bollow <nb@bollow.ch>
2021-05-01 14:42:36 +0200

    new 'trade facilitation' page

---
+++ theme-page/trade-facilitation.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php
+<?php
+$html_title="Trade Facilitation";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_theme="Trade Facilitation";
+include 'include/sidebar_themes.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title">Trade Facilitation</h1>
+        </div> <!-- /#content-header -->
+
+        <div id="content-area">
+
+  <i><a href="https://www.twn.my/title2/wto.info/2017/ti170305.htm">WTO Trade Facilitation Agreement enters into force</a></i>, by Kanaga Raja. (3 March 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2016/ti160305.htm">TFA ratified by 70 WTO members to date, PCTF told</a></i>, by Kanaga Raja. (11 March 2016)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2015/ti151013.htm">Less than half of total to bring TFA into force</a></i>, by Kanaga Raja. (20 October 2015)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2014/ti140905.htm">WTO TF PrepCom to meet on 29 September</a></i>, by Kanaga Raja. (25 September 2014)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2014/ti140806.htm">Supports announced for WTO TFA insufficient or misdirected</a></i>, by Jeronim Capaldo. (4 August 2014)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2014/ti140804.htm">Trade facilitation may result in trade hallucination</a></i>, by Jeronim Capaldo. (4 August 2014)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2014/ti140801.htm">Misplaced claims of deadline sanctity for TFA</a></i>, by Chakravarthi Raghavan. (1 August 2014)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2014/ti140709.htm">TFA's entry must be conditioned on development outcomes</a></i>, by Kanaga Raja. (21 July 2014)  
+
+ 
+<h2><a href="/o/en/themes/3105">Previous work</a></h2>
+For previous work, please see
+<a href="/o/en/themes/3105">here</a>.
+<p>
+&nbsp;
+<p>
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
