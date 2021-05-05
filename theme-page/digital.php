cb3f9cec6950a176011e67293d079e2e1a4e3c13
Norbert Bollow <nb@bollow.ch>
2021-05-05 15:32:21 +0200

    added 'updates on the negotiations' links from 2021-04-23

--- theme-page/digital.php
+++ theme-page/digital.php
@@ -24,7 +24,7 @@ Some countries are pushing for anti-development, anti-public interest and pro-Bi
 <b>Table of contents:</b>
 <ol>
   <li><a href="#1">Statements from civil society</a>
-  <li><a href="#2">Key updates</a>
+  <li><a href="#2">Key new reports</a>
   <li><a href="#3">Introductory articles</a>
   <li><a href="#4">General Analysis of problems with the proposed negotiations on digital trade</a>
   <li><a href="#5">Detailed analysis of problems with specific proposed provisions on digital trade</a>
@@ -170,40 +170,40 @@ French:
 
 <a href="/2019_WTO_PF_95">Future of WTO in a digitalised world</a>.
 Video recording of session #95 at the 2019 WTO Public Forum.
-(10 October 2019)
+(10 October 2019)
 <p>
 <i>Why the dominant digital trade paradigm will not work for women in the global south:</i>
 <a href="https://itforchange.net/node/1635">English</a>
 <a href="https://ourworldisnotforsale.net/2019/ITfC_Women.pdf">pdf</a>,
 <a href="https://ourworldisnotforsale.net/2019/ITfC_Women_ES.pdf">Spanish</a>,
 <a href="https://ourworldisnotforsale.net/2019/ITfC_Women_FR.pdf">French</a>.
-(April 2019)
+(April 2019)
 <p>
 <i>E-commerce and MSMEs: what trade rules could improve the business climate in Africa?</i>
 English: <a href="https://ourworldisnotforsale.net/2019/Munu_Africa.pdf">pdf</a>, <a href="https://ourworldisnotforsale.net/2019/Munu_Africa.docx">docx</a>.
 Spanish: <a href="https://ourworldisnotforsale.net/2019/Munu_Africa_ES.pdf">pdf</a>, <a href="https://ourworldisnotforsale.net/2019/Munu_Africa_ES.docx">docx</a>.
 French: <a href="https://ourworldisnotforsale.net/2019/Munu_Africa_FR.pdf">pdf</a>, <a href="https://ourworldisnotforsale.net/2019/Munu_Africa_FR.docx">docx</a>.
-(March 2019)
+(March 2019)
 <p>
 <i><a href="/2018/RLS_Africa.pdf">Why E-Commerce won’t work for Africa’s development.</a></i>
-(December 2018)
+(December 2018)
 <p>
 <i>Micro, small, and medium-sized enterprises (MSMEs) and e-commerce:</i>
 English:
 <a href="https://ourworldisnotforsale.net/2017/Hill_MSMEs_E-commerce.pdf">pdf</a>,
 <a href="https://ourworldisnotforsale.net/2017/Hill_MSMEs_E-commerce.docx">docx</a>.
-(December 2017)
+(December 2017)
 <p>
 <i><a href="https://ourworldisnotforsale.net/2017/Kelsey_E-commerce.pdf">E-commerce: the development implications of future proofing global trade rules for GAFA</a></i>.
-(December 2017)
+(December 2017)
 <p>
 <i><a href="https://ourworldisnotforsale.net/2017/TWN_E-commerce.pdf">Preliminary analysis of aspects of some WTO ecommerce proposals</a></i>.
-(10 December 2017)
+(10 December 2017)
 <p>
 <i>The fourth industrial revolution will deepen the digital divide.</i> Alliance Sud Position on the Eleventh WTO Ministerial Conference:
 <a href="https://ourworldisnotforsale.net/2017/AS_Digital_divide.pdf">English</a>,
 <a href="https://ourworldisnotforsale.net/2017/AS_Digital_divide_FR.pdf">French</a>.
-(30 November 2017)
+(30 November 2017)
 
 <h2 id="5">Detailed analysis of problems with specific proposed provisions on digital trade</h2>
 
@@ -253,26 +253,10 @@ UNCTAD report: <i><a href="https://ourworldisnotforsale.net/2017/UNCTAD_competit
 <a href="https://www.sef-bonn.org/en/publications/global-governance-spotlight/42019.html"><i>Digital public goods: A precondition for realising the SDGs</i></a>.
 (September 2019)
 <p>
-<a href="http://www.unrisd.org/TechAndHumanRights-Gurumurthy-Chami"><i>Data frameworks for a right to development</i></a>.
-(March 2018)
-<p>
 <i>Data localization: A matter of rule of law and economic development:</i>
 <a href="https://ourworldisnotforsale.net/2018/ITfC_Data_localisation.pdf">English</a>,
 <a href="https://ourworldisnotforsale.net/2018/ITfC_localisation_donnees.pdf">French</a>.
 (September 2018)
-<p>
-<i>Policies for the platform economy</i>:
-<a href="https://ourworldisnotforsale.net/2018/ITfC_Platforms.pdf">English</a>,
-<a href="https://ourworldisnotforsale.net/2018/ITfC_plateforme.pdf">French</a>.
-(September 2018)
-<p>
-<i>Digital industrialisation in developing countries</i>: <a href="https://ourworldisnotforsale.net/2018/ITfC_Digital_industrialisa.pdf">English</a>,
-<a href="https://ourworldisnotforsale.net/2018/ITfC_ind_num_pd.doc">French summary</a>.
-(December 2017)
-<p>
-UNCTAD report: 
-<i><a href="https://unctad.org/webflyer/south-south-digital-cooperation-industrialization-regional-integration-agenda">South-south digital cooperation for industrialization: A regional integration agenda</a></i>.
-(April 2018)
 <p>
 UNCTAD <i>Trade and Development Report 2018: Power, Platforms and The Free Trade Delusion:
 <a href="https://unctad.org/en/pages/PublicationWebflyer.aspx?publicationid=2227">English</a>,
@@ -284,10 +268,56 @@ UNCTAD <i>Trade and Development Report 2018: Power, Platforms and The Free Trade
       <a href="https://unctad.org/en/PublicationChapters/tdr2018ch3_en.pdf">English</a>,
       <a href="/2019/UNCTAD_TDR2018_chapter_III_FR.pdf">French</a>.
 <p>
+<i>Policies for the platform economy</i>:
+<a href="https://ourworldisnotforsale.net/2018/ITfC_Platforms.pdf">English</a>,
+<a href="https://ourworldisnotforsale.net/2018/ITfC_plateforme.pdf">French</a>.
+(September 2018)
+<p>
+UNCTAD report: 
+<i><a href="https://unctad.org/webflyer/south-south-digital-cooperation-industrialization-regional-integration-agenda">South-south digital cooperation for industrialization: A regional integration agenda</a></i>.
+(April 2018)
+<p>
+<a href="http://www.unrisd.org/TechAndHumanRights-Gurumurthy-Chami"><i>Data frameworks for a right to development</i></a>.
+(March 2018)
+<p>
 <i><a href="https://ourworldisnotforsale.net/2017/TWN_Procurement.pdf">Some of the implications of ecommerce proposals for government procurement</a></i>
 (10 December 2017)
+<p>
+<i>Digital industrialisation in developing countries</i>: <a href="https://ourworldisnotforsale.net/2018/ITfC_Digital_industrialisa.pdf">English</a>,
+<a href="https://ourworldisnotforsale.net/2018/ITfC_ind_num_pd.doc">French summary</a>.
+(December 2017)
 
 <h2 id="7">Updates and articles on the negotiations</h2>
+  <i><a href="https://www.twn.my/title2/wto.info/2021/ti210307.htm">US warns over moves to discontinue moratorium on e-commerce duties</a></i>, by D. Ravi Kanth. (5 March 2021)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2021/ti210306.htm">Proponents fail to respond to challenge over legal status of JSIs</a></i>, by D. Ravi Kanth. (4 March 2021)  
+<p>
+<i><a href="https://twn.my/title2/wto.info/2021/ti210217.htm">JSIs will erode WTO's multilateral rules-based architecture</a></i>, by D. Ravi Kanth. (24 February 2021) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2021/ti210213.htm">UNCTAD cautions against JSI e-commerce negotiations at WTO</a></i>, by D. Ravi Kanth. (18 February 2021) 
+<p>
+<i><a href="https://twn.my/title2/wto.info/2021/ti210202.htm">South faces serious economic implications from JSI digital trade rules</a></i>, by D. Ravi Kanth. (4 February 2021) 
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200912.htm">JSI group on e-com to discuss open internet access, competition issues</a></i>, by D. Ravi Kanth. (15 September 2020) 
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200805.htm">Renewed JSI efforts to extend e-com duties moratorium to services</a></i>, by D. Ravi Kanth. (6 August 2020) 
+<p>
+ <i><a href="https://twn.my/title2/latestwto/general/News/Digital%20Tax.pdf">How ‘Digital Trade’ Rules Would Impede Taxation of the Digitalised Economy in the Global South</a></i>, by Jane Kelsey, John Bush, Manuel Montes &amp; Joy Ndubai (August 2020)  
+<p>
+ <i><a href="https://twn.my/title2/latestwto/general/Analytical/Moratorium_tariffs_Montes_Kelsey_21072020.pdf">Moratorium on tariffs on Electronic Transmissions</a></i>, by Manuel Montes &amp; Jane Kelsey (August 2020)  
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200721.htm">Reconsider WTO e-com duties moratorium, say two scholars</a></i>, by D. Ravi Kanth. (28 July 2020) 
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200711.htm">&quot;Robust&quot; debate over damage caused by e-com duties moratorium</a></i>, by D. Ravi Kanth. (16 July 2020) 
+<p>
+ <i><a href="https://twn.my/title2/wto.info/2020/ti200710.htm">OECD suffers setback on continuation of e-com duties moratorium</a></i>, by D. Ravi Kanth. (15 July 2020)  
+<p>
+ <i><a href="https://twn.my/title2/wto.info/2020/ti200707.htm">JSI group voices differing views on moratorium on e-com duties</a></i>, by D. Ravi Kanth. (7 July 2020)  
+<p>
+ <i><a href="https://twn.my/title2/wto.info/2020/ti200705.htm">Battle-lines being drawn at WTO over moratorium on e-com duties</a></i>, by D. Ravi Kanth. (3 July 2020)  
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200617.htm">JSI group on e-com discusses a &quot;digital trade facilitation&quot; agreement</a></i>, by D. Ravi Kanth. (19 June 2020) 
+<p>
 <i><a href="https://www.twn.my/title2/wto.info/2020/ti200601.htm">US to target countries that impose digital services taxes</a></i>.
 (4 June 2020)
 <p>
@@ -297,11 +327,95 @@ UNCTAD <i>Trade and Development Report 2018: Power, Platforms and The Free Trade
 <i><a href="https://www.twn.my/title2/wto.info/2020/ti200502.htm">COVID-19: JSI group for exploring emerging e-com/digital trade issues</a></i>.
 (4 May 2020)
 <p>
+ <i><a href="https://twn.my/title2/wto.info/2020/ti200503.htm">E-com proposals at WTO a recipe for corporate greed</a></i>, by Kanaga Raja. (5 May 2020)  
+<p>
+ <i><a href="https://twn.my/title2/wto.info/2020/ti200502.htm">COVID-19: JSI group for exploring emerging e-com/digital trade issues</a></i>, by D. Ravi Kanth. (4 May 2020)  
+<p>
+<i><a href="https://twn.my/title2/wto.info/2020/ti200211.htm">US wants website owners shielded over content in e-com pluri-accord</a></i>, by D. Ravi Kanth. (24 February 2020) 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2019/ti191118.htm">Pluri-talks on e-commerce takes up controversial issues</a></i>, by D. Ravi Kanth. (27 November 2019)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2019/ti191117.htm">US offers quid pro quo on e-commerce moratorium</a></i>, by D. Ravi Kanth. (27 November 2019)  
+<p>
 <i><a href="https://www.twn.my/title2/wto.info/2019/ti190606.htm">Moratorium on e-com duties will negatively impact South</a></i>.
-(June 12, 2019)
+(23 June 2019)
 <p>
 <i><a href="https://www.twn.my/title2/wto.info/2019/ti190501.htm">“Digital trade” war underway in e-com pluri-talks at WTO</a></i>.
-(May 6, 2019)
+(6 May 2019)
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2019/ti190217.htm">India: Publishes draft e-commerce policy document, open for comments</a></i>, by Chakravarthi Raghavan. (27 February 2019) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2019/ti190205.htm">Beware Trojan horse of proposed E-commerce rules</a></i>, by IPS/Chakravarthi Raghavan &amp; Jomo Kwame Sundaram. (12 February 2019) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2019/ti190112.htm">What are trade rules on e-commerce supposed to do?</a></i>, by Chakravarthi Raghavan. (31 January 2019) 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti181213.htm">ICs still planning to launch e-com pluri-talks at Davos</a></i>, by D. Ravi Kanth. (21 December 2018)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2018/ti181212.htm">More questions than answers at Africa e-Com event</a></i>, by Chakravarthi Raghavan. (21 December 2018)  
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2018/ti181113.htm">China comes out against ambitious rules on e-commerce</a></i>, by D. Ravi Kanth. (22 November 2018) 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171234.htm">Azevedo accountable to GC on Alibaba-WEF-DG e-com initiative</a></i>, by Roberto Bissio. (21 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171233.htm">WTO teaming up with Alibaba, WEF to &quot;enable&quot; e-com may be illegal</a></i>, by Robert Howse. (18 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171232.htm">North and its allies in South opt for plurilaterals on E-com and MSMEs</a></i>, by D. Ravi Kanth. (15 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171225.htm">MC11 ending amidst endless frustration of large majority of South</a></i>, by D. Ravi Kanth. (14 December 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171224.htm">With US intransigent, MC11 may end with Chair statement</a></i>, by D. Ravi Kanth. (14 December 2017)  
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti171220.htm">CSOs highlight dangers of pursuing e-com agenda in trade talks</a></i>, by Kanaga Raja. (12 December 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti171127.htm">MC11 e-com battlelines drawn across three camps</a></i>, by Parminder Jeet Singh. (30 November 2017) 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171121.htm">South nations throw down gauntlet on e-com at MC11</a></i>, by D. Ravi Kanth. (24 November 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171120.htm">E-Commerce at WTO ministerial - effort to hijack basic internet governance issues</a></i>, by Chakravarthi Raghavan. (22 November 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171119.htm">US cherry-picking MC11 outcomes, holds Members hostage</a></i>, by D. Ravi Kanth. (21 November 2017)  
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti171021.htm">The African Group position on e-commerce talks at WTO</a></i>, by Kanaga Raja. (26 October 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti171015.htm">South countries shoot down proposals for e-commerce negotiations</a></i>, by D. Ravi Kanth. (19 October 2017) 
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171006.htm">African Group will not negotiate e-commerce rules at WTO</a></i>, by D. Ravi Kanth. (10 October 2017)  
+<p>
+ <i><a href="https://www.twn.my/title2/wto.info/2017/ti171005.htm">Opportunities, costs and risks in roll-out of digital economy</a></i>, by Kanaga Raja. (10 October 2017)  
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170917.htm">ICs and allies in South push for new mandate on e-commerce</a></i>, by D. Ravi Kanth. (28 September 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170723.htm">Developing nations and LDCs reject new E-com work programme</a></i>, by D. Ravi Kanth. (28 July 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170612.htm">North to try to bury DDA, push e-commerce &amp; MSME talks at MC11</a></i>, by D. Ravi Kanth. (16 June 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170501.htm">Friends of E-Commerce lay out roadmap for MC11 and beyond</a></i>, by Kanaga Raja. (2 May 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170405.htm">Resistance to accelerated talks on fisheries subsidies, e-commerce</a></i>, by D. Ravi Kanth. (10 April 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170313.htm">ICs intensify efforts on e-commerce negotiations at MC11</a></i>, by D. Ravi Kanth. (14 March 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170306.htm">WTO secretariat pushing e-commerce and investment for G20 meet</a></i>, by D. Ravi Kanth. (3 March 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2017/ti170214.htm">14 developing countries meet DG, push e-commerce</a></i>, by D. Ravi Kanth. (21 February 2017) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti161113.htm">GC chair ignores core development issues, pushes e-commerce</a></i>, by D. Ravi Kanth. (15 November 2016) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti161021.htm">Africa group's deadly blow to E-com thematic or negotiating issues talks</a></i>, by D. Ravi Kanth. (25 October 2016) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti161016.htm">North's efforts to &quot;bury&quot; Doha, push e-commerce and GVCs at MC11</a></i>, by D. Ravi Kanth. (18 October 2016) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti161014.htm">US, EU foisting e-commerce agenda at WTO, amidst disarray in South</a></i>, by D. Ravi Kanth. (17 October 2016) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti160803.htm">Concerted move by EU, US and allies to rewrite e-com work program</a></i>, by D. Ravi Kanth. (4 August 2016) 
+<p>
+<i><a href="https://www.twn.my/title2/wto.info/2016/ti160705.htm">Deliver on &quot;Cotton issue&quot; before e-commerce talks, WTO told</a></i>, by D. Ravi Kanth. (12 July 2016) 
+<p>
+    
+<p>
+  
+
+
 <p>
 &nbsp;
 <p>
