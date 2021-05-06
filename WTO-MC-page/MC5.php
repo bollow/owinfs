3613937d1625dc45a468e502e877d5ad70ceb99c
Norbert Bollow <nb@bollow.ch>
2021-05-06 09:42:59 +0200

    new MC5 page

---
+++ WTO-MC-page/MC5.php
@@ -0,0 +1,136 @@
+#!/usr/bin/php
+<?php
+$html_title="5th Ministerial, Cancun 2003";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_mc="5th Ministerial, Cancun 2003";
+include 'include/sidebar_ministerials.php';
+include 'include/navbar.php';
+?>
+      <div id="content" class="singlecolumn">
+        <div id="content-header">
+          <h1 class="title">5<sup>th</sup> Ministerial, Cancun 2003</h1>
+        </div> <!-- /#content-header -->
+
+      <div id="content-area">
+<i><a href="/o/en/article/general-council-meeting-ends-no-breakthrough.html">General council meeting ends with 'no breakthrough'</a></i>, by Martin Khor.
+(17 December 2003)
+<p>
+<i><a href="/o/en/article/herunder-position-cpe-after-failure-wto-negotiations-cancun.html">Failure of WTO negociations in Cancun</a></i>.
+(16 September 2003)
+<p>
+<i><a href="/o/en/article/tequila-sunset-cancn.html">Tequila sunset in Cancun</a></i>.
+(16 September 2003)
+<p>
+<i><a href="/o/en/article/cancn-segundo-seattle-omc.html">Cancún: el segundo Seattle de la OMC</a></i>.
+(16 September 2003)
+<p>
+<i><a href="/o/en/article/after-cancun-rich-mans-debacle-poor-mans-moral-victory.html">After Cancun, Rich Man's 'Debacle' Is the Poor Man's 'Moral Victory'</a></i>, by Jefferson Morley.
+(15 September 2003)
+<p>
+<i><a href="/o/en/article/comments-draft-cancun-ministerial-text.html">Comments on the draft Cancun ministerial text</a></i>, by Martin Khor.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/cancn-meeting-will-end-without-agreement-ministerial-text.html">The Cancun meeting will end without an agreement on the ministerial text</a></i>, by Martin Khor.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/some-key-issues-cancun.html">Some key issues in Cancun</a></i>, by Martin Khor.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/wto-derailed-second-seattle-cancun.html">WTO derailed at 'Second Seattle' in Cancun</a></i>, by Peter Rosset.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/trade-and-competition-policy-wto.html">Trade and competition policy in the WTO</a></i>, by Cecilia Oh.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/wto-singapore-issues-whats-stake-and-why-it-matters.html">WTO 'Singapore issues': What's at stake and why it matters</a></i>, by Martin Khor.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/fate-ministerial-hangs-thread-today-developing-countries-express-frustration-text.html">Fate of ministerial hangs on a thread today — as developing countries express frustration with the text</a></i>, by Martin Khor.
+(13 September 2003)
+<p>
+<i><a href="">The new 'deal' on TRIPS and drugs: What does it mean for access to medicines?</a></i>, by Cecilia Oh.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/african-cotton-producers-are-upset.html">African cotton producers are upset</a></i>.
+(13 September 2003)
+<p>
+<i><a href="/o/en/article/leaked-eu-draft-exposes-reality-behind-development-rethoric.html">Leaked EU draft exposes reality behind 'development rethoric'</a></i>, by Roberto Bissio.
+(11 September 2003)
+<p>
+<i><a href="/o/en/article/wto-us-demands-poor-countries-pay-agricultural-subsidy-reduction.html">WTO: The US demands poor countries to 'pay' for agricultural subsidy reduction</a></i>, by Roberto Bissio.
+(11 September 2003)
+<p>
+<i><a href="/o/en/article/singapore-issues-no-negotiations-say-70-developing-countries.html">Singapore issues: No negotiations, say 70 developing countries</a></i>, by Martin Khor.
+(10 September 2003)
+<p>
+<i><a href="/o/en/article/brazil-upbeat-agriculture-outcome-acp-group-confirm-their-stand-singapore-issues.html">Brazil upbeat on agriculture outcome, ACP Group confirm their stand on Singapore issues</a></i>, by Cecilia Oh.
+(10 September 2003)
+<p>
+<i><a href="/o/en/article/cancun-ministerial-starts-shadow-protests-and-untransparent-process.html">Cancun ministerial starts in shadow of protests and an untransparent process</a></i>, by Martin Khor.
+(10 September 2003)
+<p>
+<i><a href="/o/en/article/historic-cartagena-biosafety-protocol-comes-effect.html">Historic Cartagena biosafety protocol comes into effect</a></i>.
+(10 September 2003)
+<p>
+<i><a href="/o/en/article/protesters-march-cancun.html">Protesters march on Cancun</a></i>.
+(9 September 2003)
+<p>
+<i><a href="/o/en/article/greetings-everyone-wto-meeting-and-social-movements-resistance.html">Greetings To Everyone From The WTO Meeting And Social Movements Resistance</a></i>.
+(9 September 2003)
+<p>
+<i><a href="/o/en/article/peasant-farmers-show-strength-cauldron-grassroots-politics.html">Peasant farmers show strength in cauldron of grassroots politics</a></i>, by John Vidal.
+(9 September 2003)
+<p>
+<i><a href="/o/en/article/korean-farmer-takes-own-life-amid-protests-cancn.html">Korean farmer takes own life amid protests in Cancun</a></i>.
+(9 September 2003)
+<p>
+<i><a href="/o/en/article/wto-should-take-look-cancuns-back-yard.html">The WTO should take a look in Cancun's back yard</a></i>.
+(8 September 2003)
+<p>
+<i><a href="/o/en/article/agriculture-becomes-centerpiece.html">Agriculture becomes the centerpiece-</a></i>, by Raghav Narsalay.
+(8 September 2003)
+<p>
+<i><a href="/o/en/article/developing-countries-prepare-agriculture-battle-cancun-ministerial.html">Developing countries prepare for agriculture battle at Cancun ministerial</a></i>, by Martin Khor.
+(8 September 2003)
+<p>
+<i><a href="/o/en/article/india-steps-attack-us-eu-ahead-wto-meet.html">India steps up attack on US, EU ahead of WTO meet</a></i>, by KR Sudhaman.
+(8 September 2003)
+<p>
+<i><a href="/o/en/article/ministers-limber-wto-subsidies-battle.html">Ministers limber up for WTO subsidies battle</a></i>, by Guy de Jonquieres, Frances Williams and John Authers.
+(8 September 2003)
+<p>
+<i><a href="/o/en/article/mexican-peasant-leaders-plan-cancun-protest.html">Mexican peasant leaders plan Cancun protest</a></i>,
+by John Authers and Sara Silver.
+(7 September 2003)
+<p>
+<i><a href="/o/en/article/trade-talk-speaking-tongues.html">Trade talk: Speaking in tongues</a></i>, by Aziz Choudry.
+(5 September 2003)
+<p>
+<i><a href="/o/en/article/draft-cancn-ministerial-text-revisionmarket-access-non-agricultural-products.html">Draft Cancun ministerial text - Revision/market access for non-agricultural products</a></i>.
+(3 September 2003)
+<p>
+<i><a href="/o/en/article/palermo-cancun-eu-trade-ministers-asked-stop-wto-expansion.html">From Palermo to Cancun: EU trade ministers asked to stop WTO expansion</a></i>.
+(2 July 2003)
+<p>
+<i><a href="/o/en/article/decision-making-process-and-single-undertaking.html">The decision-making process and the single undertaking</a></i>, by Shefali Sharma.
+(16 June 2003)
+<p>
+<i><a href="/o/en/article/restricted-wto-documents-outline-non-transparent-cancun-preparatory-process-0.html">Restricted WTO documents outline non-transparent Cancun preparatory process</a></i>.
+(28 May 2003)
+<p>
+<i><a href="/o/en/article/coherence-between-world-bank-imf-and-wto-flawed-agenda-development.html">Coherence Between World Bank, IMF And WTO: A flawed agenda for development</a></i>.
+(19 May 2003)
+<p>
+<i><a href="/o/en/article/cancun-promises-be-problematic-if-decisions-left-ministerial.html">Cancun promises to be problematic if decisions left for ministerial</a></i>.
+(28 April 2003)
+<p>
+
+      </div> <!-- /#content-area -->
+    </div> <!-- /#content -->
+  </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
