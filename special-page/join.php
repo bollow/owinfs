05c1446857ee90060da41781eaab7526fe13cdb9
Norbert Bollow <nb@bollow.ch>
2021-05-12 11:17:48 +0200

    new 'join' page with a fixed link

---
+++ special-page/join.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+$html_title="Becoming an OWINFS member | Our World Is Not For Sale";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_aboutpage="Becoming an OWINFS member";
+include 'include/sidebar_aboutpage.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title">Becoming an OWINFS member</h1>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+OWINFS welcomes organizations with similar goals and activities to become members! To become a member of OWINFS, we ask that organizations first endorse our <a href="statement-political-unity">Statement of Political Unity</a>. Then just <a href="mailto:djames@cepr.net">email OWINFS </a> a one page description of your work, your membership base, and why you would like to become a member of OWINFS. Organizations should include a current OWINFS member as a reference.</p>Then, we circulate this document to our members and if there are no objections, after two weeks, you become a member! This allows you to be on our confidential information and strategizing listservs and participate in the network. We look forward to working with you!
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
