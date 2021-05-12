6a1ba65baef862798b0bfab4aafd86dca30a0ba4
Norbert Bollow <nb@bollow.ch>
2021-05-12 11:19:01 +0200

    new template for aboutpages

---
+++ special-page/template.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+$html_title="";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$this_aboutpage="";
+include 'include/sidebar_aboutpage.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+
+        <div id="content-header">
+          <h1 class="title"></h1>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+
+        </div> <!-- /#content-area -->
+      </div> <!-- /#content -->
+    </div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
