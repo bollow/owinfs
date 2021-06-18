e803e171926bb14c0295ef0b3a84ca381ab1d1d1
Norbert Bollow <nb@bollow.ch>
2021-06-18 16:35:31 +0200

    Leterme article added

---
+++ contentitem-page/2021-06-10_Leterme.php
@@ -0,0 +1,62 @@
+#!/usr/bin/php
+<?php
+$html_title="La régulation du numérique est un enjeu trop important pour être confié à l’Organisation mondiale du commerce";
+include 'include/head_etc.php'
+?>
+    <div id="main" class="group">
+<?php
+$sidebar_title="About us";
+include 'include/sidebar_about.php';
+include 'include/navbar.php';
+?>
+
+      <div id="content" class="singlecolumn">
+        <div id="content-header">
+          <h1 class="title">La régulation du numérique est un enjeu trop important pour être confié à l’Organisation mondiale du commerce</h1>
+	  <h4>par Cédric Leterme, juin 2021</h4>
+        </div> <!-- /#content-header -->
+        
+        <div id="content-area">
+Depuis janvier 2019, environ quatre-vingts Etats négocient en toute discrétion un accord sur la libéralisation du « commerce électronique » dans le cadre de l’Organisation mondiale du commerce (OMC). A l’heure où la régulation des géants du numérique s’impose comme une nécessité économique et démocratique, les règles en discussion leur conféreraient au contraire encore plus de pouvoir. La prochaine conférence ministérielle de l’OMC doit être l’occasion de mettre un terme définitif à ces négociations, aussi dangereuses qu’illégales.
+<p>
+Les problèmes que pose la concentration inédite de pouvoir économique et politique aux mains de quelques plates-formes numériques géantes commencent à être largement documentés : abus de position dominante, évasion fiscale, atteinte à la vie privée, ubérisation du travail, campagnes de désinformation, etc.
+<p>
+Même aux Etats-Unis et en Chine, qui abritent à eux seuls l’écrasante majorité des plus grosses plates-formes du monde, la situation commence à inquiéter des autorités jusqu’ici réputées conciliantes. Pékin multiplie ainsi les enquêtes et les sanctions depuis plusieurs mois contre ses géants nationaux, tandis qu’aux Etats-Unis, des procès historiques se préparent contre la plupart des GAFA [Google, Apple, Facebook et Amazon].
+<p>
+En Europe aussi, l’heure est au tour de vis réglementaire, avec notamment les projets de Digital Services Act (DSA) et de Digital Market Act (DMA) qui visent explicitement, selon la Commission européenne, à « mettre de l’ordre dans le chaos » et à s’assurer que « notre démocratie rattrape la technologie ».
+
+<h2>En toute discrétion</h2>
+
+Et pourtant… A l’heure où la régulation du numérique semble s’imposer comme une nécessité économique et démocratique, les mêmes Etats négocient en toute discrétion des règles commerciales internationales qui aboutiraient à donner encore plus de pouvoir aux plates-formes !
+<p>
+Lancées en 2019 en marge du Forum de Davos, ces négociations portent officiellement sur le « commerce électronique », mais leur portée réelle va bien au-delà.
+<p>
+Parmi les règles en discussion, on retrouve par exemple des clauses sur la libre circulation des données à travers les frontières, l’interdiction des mesures de localisation des données, ou la protection des codes sources et des algorithmes. Des dispositions qui émanent directement des principaux lobbys du numérique – aujourd’hui les plus gros dépensiers en termes de lobbying politique – et qui visent à verrouiller la capacité actuelle des grandes plates-formes à se développer à l’échelle de la planète avec un minimum de contrôles et de contraintes.
+<p>
+Article réservé à nos abonnés Lire aussi La France veut durcir la régulation européenne des plates-formes numériques 
+<p>
+Pour ces multinationales, il s’agit en particulier de se prémunir contre un double risque. Celui, d’abord, de voir des Etats, notamment au Sud, multiplier les mesures de protectionnisme et plus largement de souveraineté numériques pour tenter de développer leur propre industrie ou, a minima, de limiter leur dépendance envers les principaux acteurs internationaux du secteur.
+<p>
+Celui, ensuite, de voir les débats actuels sur le pouvoir excessif des plates-formes déboucher sur des régulations encore plus contraignantes que celles qui sont envisagées.
+Négociations dangereuses
+On comprend dès lors l’intérêt d’en passer par l’OMC, une des rares institutions internationales dotées de pouvoirs contraignants, qui plus est entièrement vouée à la « libéralisation commerciale » au détriment des autres considérations (sociales, environnementales, démocratiques) et particulièrement perméable aux influences des pays riches et des multinationales.
+<p>
+Preuve de la dangerosité de ces négociations, la moitié des membres de l’OMC ont refusé de les rejoindre et s’opposent même à leur principe (tout comme plusieurs centaines d’organisations de la société civile internationale), ce qui, en passant, les rend illégales puisque toutes les nouvelles négociations à l’OMC doivent normalement être approuvées à l’unanimité.
+<p>
+Qu’à cela ne tienne, les partisans de ces négociations controversées, dont l’Union européenne (UE), comptent bien les mener à terme et tenter d’en imposer les résultats lors de la prochaine conférence ministérielle qui aura lieu en fin d’année, à Genève. Contre ce coup de force, il est donc urgent de rappeler à l’UE et à ses Etats membres qu’ils ne peuvent défendre à l’OMC ce qu’ils critiquent sur leur propre sol.
+<p>
+La régulation du numérique est un enjeu beaucoup trop important pour être confié à l’OMC. Le pouvoir des plates-formes doit être limité, pas renforcé. Et l’encadrement du numérique doit faire l’objet d’un véritable débat démocratique. Un débat qui risque bien d’être largement privé d’objet si cet accord sur le « commerce électronique » finit par voir le jour.
+<p>
+&nbsp;
+<p>
+Cédric Leterme est chargé d’étude au Centre tricontinental (Cetri) et au Groupe de recherche pour une stratégie économique alternative (Gresea).
+<p>
+&nbsp;
+<p>
+Publié initialement le 10 juin 2021: <a href="https://www.lemonde.fr/idees/article/2021/06/10/la-regulation-du-numerique-est-un-enjeu-trop-important-pour-etre-confie-a-l-organisation-mondiale-du-commerce_6083546_3232.html">Le Monde</a>.
+</div> <!-- /#content-area -->
+</div> <!-- /#content -->
+</div> <!-- /#main -->
+<?php
+include 'include/foot_etc.php'
+?>
