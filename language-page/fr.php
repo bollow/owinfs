2247fa28b204eafc9b7883cb4abf6a5a051ba101
Norbert Bollow <nb@bollow.ch>
2021-06-18 16:08:45 +0200

    French-language mediat article on disaccreditation issue: Link update and move to “other topics”, as requested 2021-06-18

--- language-page/fr.php
+++ language-page/fr.php
@@ -15,11 +15,6 @@ include 'include/navbar_fr.php';
 include 'include/article_links.php';
 article_links("data/articles", "fr");
 ?>
-        <h1>Couverture médiatique en français</h1>
-	<ol>
-          <li><a href="https://mobile2.tdg.ch/articles/5a2d6944ab5c372cf1000001">Des ONG ont été
-	      refoulées à la réunion de l'OMC</a>, <i>Tribune de Genève</i></li>
-	</ol>
       </div> <!-- /#content -->
     </div> <!-- /#main -->
 <?php
